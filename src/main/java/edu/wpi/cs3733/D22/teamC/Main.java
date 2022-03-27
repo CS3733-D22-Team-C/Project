a5b0dfc872dccbc2d01792f5e111616e41605a13
NickFrangie <51765298+NickFrangie@users.noreply.github.com>
2022-03-27 17:31:40 -0400

    Project Initialization

---
+++ src/main/java/edu/wpi/cs3733/D22/teamC/Main.java
@@ -0,0 +1,8 @@
+package edu.wpi.cs3733.D22.teamC;
+
+public class Main {
+
+  public static void main(String[] args) {
+    App.launch(App.class, args);
+  }
+}
---
+++ src/test/java/edu/wpi/cs3733/D22/teamC/DefaultTest.java
@@ -0,0 +1,13 @@
+/*-------------------------*/
+/* DO NOT DELETE THIS TEST */
+/*-------------------------*/
+
+package edu.wpi.cs3733.D22.teamC;
+
+import org.junit.jupiter.api.Test;
+
+public class DefaultTest {
+
+  @Test
+  public void test() {}
+}
