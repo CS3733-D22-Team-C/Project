1d209e1119bae5aa2064e4e4a76ad433de24d2ff
njpires20 <nelsonpires0202@gmail.com>
2022-03-29 19:23:09 -0400

    Fixed Bugs and UI

--- src/main/java/edu/wpi/cs3733/D22/teamC/controller/general/serviceRequest.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/general/serviceRequest.java
@@ -2,17 +2,13 @@ package edu.wpi.cs3733.D22.teamC.controller.general;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
-import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTreeTableView;
 import edu.wpi.cs3733.D22.teamC.App;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TreeTableCell;
 import javafx.scene.control.TreeTableColumn;
 
-import java.awt.event.InputMethodEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -30,6 +26,11 @@ public class serviceRequest implements Initializable {
 
         //service request dropdown
         serviceType.getItems().add("Medical Equipment");
+        serviceType.getItems().add("Facility Maintenance");
+        serviceType.getItems().add("Lab System");
+        serviceType.getItems().add("Medicine Delivery");
+        serviceType.getItems().add("Sanitation");
+        serviceType.getItems().add("Security");
 
         //Colums for table
         TreeTableColumn ID = new TreeTableColumn("ID");
@@ -49,13 +50,27 @@ public class serviceRequest implements Initializable {
 
     }
     @FXML
-    void onSelectButton(ActionEvent event) {
-        switch (serviceType.getAccessibleText()){
+    void onSelectButton(ActionEvent event) { // TODO: Set views for every service request
+        switch (serviceType.getValue()){
             case "Medical Equipment":
                 App.instance.setView("view/service_request/medical-equipment-view.fxml");
                 break;
+            case "Facility Maintenance":
+                App.instance.setView("");
+                break;
+            case "Lab System":
+                App.instance.setView("");
+                break;
+            case "Medicine Delivery":
+                App.instance.setView("");
+                break;
+            case "Sanitation":
+                App.instance.setView("");
+                break;
+            case "Security":
+                App.instance.setView("");
+                break;
             default:
-                return;
         }
 
     }
