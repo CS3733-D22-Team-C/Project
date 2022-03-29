48543ca686e3edbceef18388284466fef899c66d
NickFrangie <51765298+NickFrangie@users.noreply.github.com>
2022-03-29 00:34:07 -0400

    Service Request Objects (#5)

---
+++ src/main/java/edu/wpi/cs3733/D22/teamC/entity/service_request/facility_maintenance/FacilityMaintenanceServiceRequest.java
@@ -0,0 +1,15 @@
+package edu.wpi.cs3733.D22.teamC.entity.service_request.facility_maintenance;
+
+import edu.wpi.cs3733.D22.teamC.entity.service_request.ServiceRequest;
+
+public class FacilityMaintenanceServiceRequest extends ServiceRequest {
+    protected String maintenanceType;   // TODO: Make Enum
+
+    public String getMaintenanceType() {
+        return maintenanceType;
+    }
+
+    public void setMaintenanceType(String maintenanceType) {
+        this.maintenanceType = maintenanceType;
+    }
+}
