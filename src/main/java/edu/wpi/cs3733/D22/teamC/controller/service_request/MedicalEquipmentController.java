61cd9d25c7e74d4fa3c6648eaea6e46216017f94
mmueting <70529094+mmueting@users.noreply.github.com>
2022-03-28 22:02:53 -0400

    Drop Down Menus set

--- src/main/java/edu/wpi/cs3733/D22/teamC/controller/service_request/MedicalEquipmentController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/service_request/MedicalEquipmentController.java
@@ -5,20 +5,24 @@ import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextArea;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 
-public class MedicalEquipmentController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MedicalEquipmentController implements Initializable {
     @FXML private TextField AssigneID;
 
     @FXML private JFXTextArea Description;
 
     @FXML private JFXComboBox<?> Location;
 
-    @FXML private JFXComboBox<?> Priority;
+    @FXML private JFXComboBox<String> Priority;
 
-    @FXML private JFXComboBox<?> Status;
+    @FXML private JFXComboBox<String> Status;
 
-    @FXML private JFXComboBox<?> equipType;
+    @FXML private JFXComboBox<String> equipType;
 
     @FXML private JFXButton goBackButton;
 
@@ -26,6 +30,25 @@ public class MedicalEquipmentController {
 
     @FXML private JFXButton submitButton;
 
+    @FXML
+    public void initialize(URL url, ResourceBundle rb) {
+        //For equipment type drop down
+        equipType.getItems().add("Bed (20)");
+        equipType.getItems().add("Recliners (6)");
+        equipType.getItems().add("Portable X-Ray");
+        equipType.getItems().add("Infusion Pumps (30)");
+
+        //For priority dropdown
+        Priority.getItems().add("Low");
+        Priority.getItems().add("Medium");
+        Priority.getItems().add("High");
+
+        //For status dropdown
+        Status.getItems().add("Blank");
+        Status.getItems().add("Processing");
+        Status.getItems().add("Done");
+
+    }
     @FXML
     void clickGoBack(ActionEvent event) {
 
