e6946e1ae9953d310231b1b331d4e58703ce66ae
Brandon Vuong <vuong.dbrandon@gmail.com>
2022-03-28 21:20:55 -0400

    Create menu bar controller

--- src/main/java/edu/wpi/cs3733/D22/teamC/controller/component/MenuBarController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/component/MenuBarController.java
@@ -4,4 +4,23 @@ import edu.wpi.cs3733.D22.teamC.App;
 import javafx.fxml.FXML;
 
 public class MenuBarController {
+    @FXML
+    protected void onMenuBarHomeButtonPress() {
+
+    }
+
+    @FXML
+    protected void onMenuBarLocationsButtonPress() {
+
+    }
+
+    @FXML
+    protected void onMenuBarServiceRequestsButtonPress() {
+
+    }
+
+    @FXML
+    protected void onMenuBarExitButtonPress() {
+        App.instance.getStage().close();
+    }
 }
