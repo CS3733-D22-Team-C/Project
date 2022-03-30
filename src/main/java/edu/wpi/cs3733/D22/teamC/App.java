a4eb976e3acad8a4f6f4414ec7e3456f8a2addfd
Brandon Vuong <vuong.dbrandon@gmail.com>
2022-03-30 13:51:38 -0400

    Update App.java

--- src/main/java/edu/wpi/cs3733/D22/teamC/App.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/App.java
@@ -15,7 +15,7 @@ public class App extends Application {
     public static App instance;
 
     // Constants
-    private final String BASE_VIEW_PATH = "view/general/base-view.fxml";
+    public static final String BASE_VIEW_PATH = "view/general/base-view.fxml";
     private final String MENU_BAR_COMPONENT_PATH = "component/menu-bar.fxml";
     private final String MEDICAL_EQUIPMENT = "view/service_request/medical-equipment-view.fxml";
     private final String SERVICE_REQUEST_SELECT = "view/general/view-service.fxml";
