7337bbd9834c5a882ad200111a54ca4781d020dc
Brandon Vuong <57996169+bdvuong@users.noreply.github.com>
2022-03-30 13:53:21 -0400

    TC-85: UI Template Design + CSS Rulesets (#16)
    * Create base-view.fxml file
    
    * Create buttons.css
    
    Holds button colors
    
    * Create menu-bar.fxml file
    
    Temp design for menu bar
    
    * Create framework to load pages onto stage
    
    * Change button text size and window rescaling
    
    - Button text size is changed to 17 from https://github.com/CS3733-D22-Team-C/Prototype-C/pull/1#pullrequestreview-923855763
    
    - Toolbar will stretch accordingly to window size
    
    * Update fxids for buttons
    
    * Create controller class for menu bar
    
    * Exit button functionality
    
    - Add a getter for the stage in order to have exit button functionality
    
    - Refactored fxids again
    
    - Add actions on button press
    
    * Create menu bar controller
    
    * Add todo statements
    
    When all pages are done paths will be changed for full menu bar functionality.
    
    Unsure if changing setView() to static is a problem. @NickFrangie
    
    * Change setView from static context
    
    Instead use App.instance.setView();
    
    * Remove duplicate import statement
    
    * Change file name
    
    * Create sanitary-services-view.fxml
    
    * Add documentation to setView()
    
    * TC-115: CSS Formatting and demo-service-request-view.fxml (#11)
    
    * TC-85: CSS File Name Change (#4)
    
    * Create base-view.fxml file
    
    * Create buttons.css
    
    Holds button colors
    
    * Create menu-bar.fxml file
    
    Temp design for menu bar
    
    * Create framework to load pages onto stage
    
    * Change button text size and window rescaling
    
    - Button text size is changed to 17 from https://github.com/CS3733-D22-Team-C/Prototype-C/pull/1#pullrequestreview-923855763
    
    - Toolbar will stretch accordingly to window size
    
    * Update fxids for buttons
    
    * Create controller class for menu bar
    
    * Exit button functionality
    
    - Add a getter for the stage in order to have exit button functionality
    
    - Refactored fxids again
    
    - Add actions on button press
    
    * Create menu bar controller
    
    * Add todo statements
    
    When all pages are done paths will be changed for full menu bar functionality.
    
    Unsure if changing setView() to static is a problem. @NickFrangie
    
    * Change setView from static context
    
    Instead use App.instance.setView();
    
    * Remove duplicate import statement
    
    * Change file name
    
    * Service Request Objects (#5)
    
    * Tc 60 (#6)
    
    * Adding Matthew's css files
    
    * Added basic UI element
    
    * Drop Down Menus set
    
    * Base UI
    
    * Base UI for Service Request
    
    * Create demo-service-request-view.fxml
    
    * Add CSS rulesets
    
    * TC-61: Locations DAO (#7)
    
    * DAOImpl Stubs
    
    * Get and Remove Queries
    
    * Update, Insert
    
    * DBManager Changes
    
    * Trimming & Testing
    
    Co-authored-by: Brian Shin <brianshin100@yahoo.com>
    
    * Change Menu Bar Properties
    
    - Change background color to #0077CC
    - Change font color to #FFFFFF
    
    * Create request-page.css
    
    * Add menu bar formatting ruleset
    
    * Remove prefHeight and prefWidth
    
    This allows for the window to automatically size itself every time we change view
    
    * Add more CSS rulesets
    
    * Fix demo-service-request-view.fmxl
    
    For now a working prototype that can be used for all other service requests.
    
    Co-authored-by: NickFrangie <51765298+NickFrangie@users.noreply.github.com>
    Co-authored-by: mmueting <70529094+mmueting@users.noreply.github.com>
    Co-authored-by: Brian Shin <brianshin100@yahoo.com>
    
    * Delete sanitary-services-view.fxml
    
    * Add Button Colors on Hover
    
    * Readjust Template Design
    
    * Update demo-service-request-view.fxml
    
    Changes reflect medical-equipment-view.fxml's current design
    
    * Bugfix
    
    @mmueting Inability to launch fixed
    
    * Update menu-bar.fxml
    
    * Update App.java
    
    Co-authored-by: NickFrangie <51765298+NickFrangie@users.noreply.github.com>
    Co-authored-by: mmueting <70529094+mmueting@users.noreply.github.com>
    Co-authored-by: Brian Shin <brianshin100@yahoo.com>

--- src/main/java/edu/wpi/cs3733/D22/teamC/App.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/App.java
@@ -15,7 +15,7 @@ public class App extends Application {
     public static App instance;
 
     // Constants
-    private final String BASE_VIEW_PATH = "view/general/base-view.fxml";
+    public static final String BASE_VIEW_PATH = "view/general/base-view.fxml";
     private final String MENU_BAR_COMPONENT_PATH = "component/menu-bar.fxml";
     private final String MEDICAL_EQUIPMENT = "view/service_request/medical-equipment-view.fxml";
     private final String SERVICE_REQUEST_SELECT = "view/general/view-service.fxml";
@@ -38,6 +38,7 @@ public class App extends Application {
         // Store window as stage
         stage = primaryStage;
       
+
         setView(SERVICE_REQUEST_SELECT);
 
         //setView(MEDICAL_EQUIPMENT);
@@ -54,6 +55,10 @@ public class App extends Application {
         log.info("Shutting Down");
     }
 
+    /**
+     * Allows us to change the view of the window
+     * @param viewFile path to the .fxml file to be displayed
+     */
     public void setView(String viewFile) {
         try {
             // Load Base Page
