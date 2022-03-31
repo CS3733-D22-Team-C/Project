dc3392c0fd1c042e79290b96b93e2aac7e2d5e80
JohnMuirhead000 <73247467+JohnMuirhead000@users.noreply.github.com>
2022-03-30 23:14:30 -0400

    added columns to stuff (#36)
    * added columns to stuff
    
    * added second two columns
    
    * Changed column width
    
    Co-authored-by: mmueting <70529094+mmueting@users.noreply.github.com>

--- src/main/java/edu/wpi/cs3733/D22/teamC/controller/general/serviceRequest.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/general/serviceRequest.java
@@ -3,20 +3,31 @@ package edu.wpi.cs3733.D22.teamC.controller.general;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTreeTableView;
+import com.jfoenix.controls.RecursiveTreeItem;
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import edu.wpi.cs3733.D22.teamC.App;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.ServiceRequest;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.ServiceRequestDAO;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.ServiceRequestDAOImpl;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.medical_equipment.MedicalEquipmentSRTable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableColumn;
 
+import javax.management.Query;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class serviceRequest implements Initializable {
 
     //Buttons
     @FXML private JFXButton selectButton;
-    @FXML private JFXTreeTableView<?> table;
+    @FXML private JFXTreeTableView<ServiceRequestTable> table;
     @FXML private JFXComboBox<String> serviceType;
 
     @FXML
@@ -32,22 +43,23 @@ public class serviceRequest implements Initializable {
         serviceType.getItems().add("Sanitation");
         serviceType.getItems().add("Security");
 
-        //Colums for table
-        TreeTableColumn ID = new TreeTableColumn("ID");
-        ID.setPrefWidth(80);
-        TreeTableColumn Type = new TreeTableColumn("Type");
-        Type.setPrefWidth(80);
-        TreeTableColumn Assignee = new TreeTableColumn("Assignee");
-        Assignee.setPrefWidth(80);
-        TreeTableColumn Status = new TreeTableColumn("Status");
-        Status.setPrefWidth(80);
-        TreeTableColumn Location = new TreeTableColumn("Location");
-        Location.setPrefWidth(80);
-        TreeTableColumn Priority = new TreeTableColumn("Priority");
-        Priority.setPrefWidth(80);
+        ServiceRequestDAO serviceRequestDAO  = new ServiceRequestDAOImpl();
+        List<ServiceRequest> serviceRequests = serviceRequestDAO.getAllServiceRequests();
 
-        table.getColumns().addAll(ID,  Type, Assignee,Status, Location, Priority);
+        ObservableList<ServiceRequestTable> METList = FXCollections.observableArrayList();
+        final TreeItem<ServiceRequestTable> root = new RecursiveTreeItem<ServiceRequestTable>(METList, RecursiveTreeObject::getChildren);
+        ServiceRequestTable.createTableColumns(table);
 
+        table.setRoot(root);
+        table.setShowRoot(false);
+
+
+
+        for (ServiceRequest serviceRequest  :  serviceRequests)
+        {
+            ServiceRequestTable serviceRequestTable = new ServiceRequestTable(serviceRequest);
+            METList.add(serviceRequestTable);
+        }
     }
     @FXML
     void onSelectButton(ActionEvent event) { // TODO: Set views for every service request
