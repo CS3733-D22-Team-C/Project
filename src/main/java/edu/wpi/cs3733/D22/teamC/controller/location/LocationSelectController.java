5b81a731f733aa6c654d33292c0c94a355cc9f1d
njpires20 <100165903+njpires20@users.noreply.github.com>
2022-03-30 23:29:14 -0400

    Tc 99 (#37)
    * Location Table Working
    
    * Location Table
    
    - not showing location
    
    * location working

--- src/main/java/edu/wpi/cs3733/D22/teamC/controller/location/LocationSelectController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/location/LocationSelectController.java
@@ -1,39 +1,49 @@
 package edu.wpi.cs3733.D22.teamC.controller.location;
 
 import com.jfoenix.controls.JFXTreeTableView;
+import com.jfoenix.controls.RecursiveTreeItem;
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+import edu.wpi.cs3733.D22.teamC.entity.LocationTable;
+import edu.wpi.cs3733.D22.teamC.entity.location.Location;
+import edu.wpi.cs3733.D22.teamC.entity.location.LocationDAO;
+import edu.wpi.cs3733.D22.teamC.entity.location.LocationDAOImpl;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.medical_equipment.MedicalEquipmentSRTable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.TreeTableView;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class LocationSelectController implements Initializable {
 
-    @FXML private JFXTreeTableView<?> table;
+    @FXML private JFXTreeTableView<LocationTable> table;
+
+    ObservableList<LocationTable> LOCList = FXCollections.observableArrayList();
+    final TreeItem<LocationTable> root = new RecursiveTreeItem<LocationTable>(LOCList, RecursiveTreeObject::getChildren);
 
     @FXML
     public void initialize(URL url, ResourceBundle rb) {
         // Constrain column sizes to the size of the table
-        table.setColumnResizePolicy(TreeTableView.CONSTRAINED_RESIZE_POLICY);
+
         //Columns for table
-        TreeTableColumn nodeID = new TreeTableColumn("Node ID");
-        nodeID.setMaxWidth(1f * Integer.MAX_VALUE * 12.5);
-        TreeTableColumn nodeType = new TreeTableColumn("Node Type");
-        nodeType.setMaxWidth(1f * Integer.MAX_VALUE * 12.5);
-        TreeTableColumn shortName = new TreeTableColumn("Short Name");
-        shortName.setMaxWidth(1f * Integer.MAX_VALUE * 13.0);
-        TreeTableColumn longName = new TreeTableColumn("Long Name");
-        longName.setMaxWidth(1f * Integer.MAX_VALUE * 12.5);
-        TreeTableColumn floor = new TreeTableColumn("Floor");
-        floor.setMaxWidth(1f * Integer.MAX_VALUE * 12.5);
-        TreeTableColumn building = new TreeTableColumn("Building");
-        building.setMaxWidth(1f * Integer.MAX_VALUE * 12.5);
-        TreeTableColumn x = new TreeTableColumn("X");
-        x.setMaxWidth(1f * Integer.MAX_VALUE * 5);
-        TreeTableColumn y = new TreeTableColumn("Y");
-        y.setMaxWidth(1f * Integer.MAX_VALUE * 5);
-        table.getColumns().addAll(nodeID, nodeType, shortName, longName, floor, building, x, y);
+        LocationTable.createTableColumns(table);
+        table.setRoot(root);
+        table.setShowRoot(false);
+
+        // Query Database
+
+        LocationDAO locationsDAO = new LocationDAOImpl();
+        List<Location> locations = locationsDAO.getAllLocations();
+
+        for(Location tempLocation : locations){
+            LOCList.add(new LocationTable(tempLocation));
+        }
     }
 }
