0aa66c06cde05a4b742a427b713ffc2af4cafad3
njpires20 <nelsonpires0202@gmail.com>
2022-03-30 21:20:27 -0400

    Fixed Column Spacing

--- src/main/java/edu/wpi/cs3733/D22/teamC/controller/location/LocationSelectController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/location/LocationSelectController.java
@@ -23,7 +23,7 @@ public class LocationSelectController implements Initializable {
         TreeTableColumn nodeType = new TreeTableColumn("Node Type");
         nodeType.setMaxWidth(1f * Integer.MAX_VALUE * 12.5);
         TreeTableColumn shortName = new TreeTableColumn("Short Name");
-        shortName.setMaxWidth(1f * Integer.MAX_VALUE * 12.5);
+        shortName.setMaxWidth(1f * Integer.MAX_VALUE * 13.0);
         TreeTableColumn longName = new TreeTableColumn("Long Name");
         longName.setMaxWidth(1f * Integer.MAX_VALUE * 12.5);
         TreeTableColumn floor = new TreeTableColumn("Floor");
@@ -31,9 +31,9 @@ public class LocationSelectController implements Initializable {
         TreeTableColumn building = new TreeTableColumn("Building");
         building.setMaxWidth(1f * Integer.MAX_VALUE * 12.5);
         TreeTableColumn x = new TreeTableColumn("X");
-        x.setMaxWidth(1f * Integer.MAX_VALUE * 12.5);
+        x.setMaxWidth(1f * Integer.MAX_VALUE * 5);
         TreeTableColumn y = new TreeTableColumn("Y");
-        y.setMaxWidth(1f * Integer.MAX_VALUE * 12.5);
+        y.setMaxWidth(1f * Integer.MAX_VALUE * 5);
         table.getColumns().addAll(nodeID, nodeType, shortName, longName, floor, building, x, y);
     }
 }
