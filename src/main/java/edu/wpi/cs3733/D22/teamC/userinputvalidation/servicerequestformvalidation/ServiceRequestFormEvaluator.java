f8d11c971917140e63f871fa2c1e6b8c03cdc458
Matthew McAlarney <67719286+MacCode7110@users.noreply.github.com>
2022-04-03 00:58:00 -0400

    Developing basic validation checks in ServiceRequestFormEvaluator

--- src/main/java/edu/wpi/cs3733/D22/teamC/userinputvalidation/servicerequestformvalidation/ServiceRequestFormEvaluator.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/userinputvalidation/servicerequestformvalidation/ServiceRequestFormEvaluator.java
@@ -2,50 +2,67 @@ package edu.wpi.cs3733.D22.teamC.userinputvalidation.servicerequestformvalidatio
 
 //Note: If user input validation expands beyond service requests forms as development continues, then user validation can be further abstracted when refactoring.
 
-public class ServiceRequestFormEvaluator {
+import edu.wpi.cs3733.D22.teamC.entity.service_request.ServiceRequestDAOImpl;
+import edu.wpi.cs3733.D22.teamC.entity.location.LocationDAOImpl;
 
-    protected String requestID;
-    protected String location;
+public class ServiceRequestFormEvaluator {
 
     public ServiceRequestFormEvaluator() {}
 
-    public ServiceRequestFormEvaluator(ServiceRequestFormEvaluator srfe)
-    {
-        this.requestID = srfe.getRequestID();
-        this.location = srfe.getLocation();
-    }
-
+    /**
+     * Determine if the assigneeID sent from a ServiceRequestCreateController object is valid through checking its length and if it exists in the Employee Table database.
+     * If the assigneeID is too short, return 1. Else if the assigneeID does not exist in the Employee Table of the database, return 2. Else, return 0.
+     * @param ID The assigneeID of the service request.
+     * @return int.
+     */
     public int getValidateAssigneeIDResult(String ID)
     {
-        if(ID.length() != 10)
+        ServiceRequestDAOImpl serviceRequestDaoVar = new ServiceRequestDAOImpl();
+
+        if(ID.length() < 10)
         {
             return 1; //Return 1 if the assigneeID is not the proper length
         }
-        else if(true) //check getservicerequest method here
+        else
         {
-
+            return 0;
         }
-        return 0;
+
+        /*
+        else if() //Return 2 if the assigneeID is not in the database, waiting on EmployeeDAO
+        {
+            return 2;
+        }
+        */
     }
 
-    public int getValidateLocationIDResult(String location)
+    /**
+     *Determine if the location ID sent from a ServiceRequestCreateController object exists in the Location Table database.
+     * If the location ID does not exist in the Location Table database, return 3. Else, return 0.
+     * @param locationID The location ID of the service request
+     * @return int
+     */
+    public int getValidateLocationIDResult(String locationID)
     {
-        return 0;
+        LocationDAOImpl locationDaoVar = new LocationDAOImpl();
+
+        if(locationDaoVar.getLocation(locationID) == null) //assumes the user inputs a location ID in the service request location field.
+        {
+            return 3;
+        }
+        else
+        {
+            return 0;
+        }
     }
 
-    public void setRequestID(String requestID) {
-        this.requestID = requestID;
-    }
+    /**
+     * Determine if all required fields of a service request are filled. The required fields for all service requests
+     * If all required fields are not filled, return 4. Else, return 0.
+     * @return
+     */
+    public int getValidateAllRequiredFieldsFilledResult()
+    {
 
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getRequestID() {
-        return requestID;
-    }
-
-    public String getLocation() {
-        return location;
     }
 }
