537057134922e7b756a788f28840f9ed5d93af0c
vagmi20 <nagavagmi.bh@gmail.com>
2022-04-03 15:12:26 -0400

    added methods to interface

--- src/main/java/edu/wpi/cs3733/D22/teamC/entity/medical_equipment/Medical_EquipmentDAO.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/entity/medical_equipment/Medical_EquipmentDAO.java
@@ -1,4 +1,14 @@
 package edu.wpi.cs3733.D22.teamC.entity.medical_equipment;
 
+import java.util.List;
+
 public interface Medical_EquipmentDAO {
+    public List<Medical_Equipment> getMedicalEquipments();
+    public Medical_Equipment getMedicalEquipment(int equipID);
+
+    public int insertMedicalEquipment(Medical_Equipment medical_equipment);
+    public boolean updateMedicalEquipment(Medical_Equipment medical_equipment);
+    public boolean deleteMedicalEquipment(Medical_Equipment medical_equipment);
+
+
 }
