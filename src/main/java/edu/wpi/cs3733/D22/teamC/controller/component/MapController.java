cf955443513a7a1ca72df84939351352e96dc335
NickFrangie <51765298+NickFrangie@users.noreply.github.com>
2022-04-03 05:32:54 -0400

    Extended Map Functionality

--- src/main/java/edu/wpi/cs3733/D22/teamC/controller/component/MapController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/component/MapController.java
@@ -3,14 +3,18 @@ package edu.wpi.cs3733.D22.teamC.controller.component;
 import edu.wpi.cs3733.D22.teamC.entity.location.Location;
 import edu.wpi.cs3733.D22.teamC.entity.location.LocationDAO;
 import edu.wpi.cs3733.D22.teamC.entity.location.LocationDAOImpl;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.transform.Scale;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -19,20 +23,17 @@ import java.util.ResourceBundle;
 
 public class MapController implements Initializable {
     protected class MapLocation {
-        public Group group;
         public Circle node;
         public Location location;
 
         public MapLocation(Location location) {
             this.location = location;
             this.node = createNode(location.getX(), location.getY());
-            this.group = new Group(this.node);
         }
 
         public MapLocation(double x, double y) {
             this.location = null;
             this.node = createNode(x, y);
-            this.group = new Group(this.node);
         }
 
         /**
@@ -42,15 +43,24 @@ public class MapController implements Initializable {
          * @return The circle drawn as the MapLocation's node.
          */
         public Circle createNode(double x, double y) {
-            return drawCircle(x, y);
+            Circle circle = drawCircle(x, y);
+            circle.setOnMouseEntered(e -> onMouseEnterNode(e, this));
+            circle.setOnMouseExited(e -> onMouseExitNode(e, this));
+            circle.setOnMouseClicked(e -> onMouseClickedNode(e, this));
+            circle.setOnMouseDragged(e -> onMouseDraggedNode(e, this));
+            return circle;
         }
     }
 
-    @FXML
-    Pane map;
+    // Constants
+    private final static double MAX_SCALE = 4f;
+    private final static double MIN_SCALE = 0.25f;
 
-    Group root = new Group();
+    // FXML
+    @FXML ScrollPane scroll;
+    @FXML Pane map;
 
+    // Variables
     protected MapLocation clickedMapLocation, hoveredMapLocation;
     protected List<MapLocation> mapLocations;
 
@@ -58,15 +68,17 @@ public class MapController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         // Testing !!!
         LocationDAO locationDAO = new LocationDAOImpl();
-        mapLocations = renderLocations(locationDAO.getAllLocations()).subList(0, 10);
+        mapLocations = renderLocations(locationDAO.getAllLocations());
         for (MapLocation mapLocation : mapLocations) {
-            root.getChildren().add(mapLocation.group);
+            map.getChildren().add(mapLocation.node);
         }
-        map.setOnMouseClicked(this::onMouseClickedMap);
 
-        root.setTranslateX(map.getPrefWidth() / 2);
-        root.setTranslateY(map.getPrefHeight() / 2);
-        map.getChildren().add(root);
+
+        map.setOnMouseClicked(this::onMouseClickedMap);
+        map.setOnScroll(this::onMouseScrollMap);
+
+        scroll.setPrefViewportHeight(500);
+        scroll.setPrefViewportWidth(500);
     }
 
     private List<MapLocation> renderLocations(List<Location> locations) {
@@ -74,11 +86,6 @@ public class MapController implements Initializable {
 
         for (Location location : locations) {
             MapLocation mapLocation = new MapLocation(location);
-
-            mapLocation.group.setOnMouseEntered(e -> onMouseEnterNode(e, mapLocation));
-            mapLocation.group.setOnMouseExited(e -> onMouseExitNode(e, mapLocation));
-            mapLocation.group.setOnMouseClicked(e -> onMouseClickedNode(e, mapLocation));
-
             mapLocs.add(mapLocation);
         }
 
@@ -98,15 +105,28 @@ public class MapController implements Initializable {
 
         protected void onMouseClickedNode(MouseEvent event, MapLocation mapLocation) {
             if (event.getButton().equals(MouseButton.PRIMARY)) {
-                if (clickedMapLocation != null) offActiveNode(clickedMapLocation);
+                // Single-Click select toggle
+                {
+                    if (clickedMapLocation != null) offActiveNode(clickedMapLocation);
 
-                if (clickedMapLocation == mapLocation) {
-                    clickedMapLocation = null;
-                } else {
-                    onActiveNode(mapLocation);
-                    clickedMapLocation = mapLocation;
+                    if (clickedMapLocation == mapLocation) {
+                        clickedMapLocation = null;
+                    } else {
+                        onActiveNode(mapLocation);
+                        clickedMapLocation = mapLocation;
+                    }
                 }
+
+                event.consume();
             }
+        }
+
+        protected void onMouseDraggedNode(MouseEvent event, MapLocation mapLocation) {
+            double offsetX = event.getX() - mapLocation.node.getCenterX();
+            double offsetY = event.getY() - mapLocation.node.getCenterY();
+
+            mapLocation.node.setCenterX(mapLocation.node.getCenterX() + offsetX);
+            mapLocation.node.setCenterY(mapLocation.node.getCenterY() + offsetY);
         }
 
         protected void onMouseClickedMap(MouseEvent event) {
@@ -118,33 +138,47 @@ public class MapController implements Initializable {
                 }
 
                 // Double-Click create new MapLocation
+                // Todo: Move to Edit-only controller
                 if (event.getClickCount() == 2) {
-                    System.out.println("Double: " + event.getX() + " " + event.getY());
                     MapLocation newMapLoc = new MapLocation(event.getX(), event.getY());
                     mapLocations.add(newMapLoc);
-                    root.getChildren().add(newMapLoc.group);
+                    map.getChildren().add(newMapLoc.node);
                 }
+            }
+        }
+
+        protected void onMouseScrollMap(ScrollEvent event) {
+            if (event.getDeltaY() != 0) {
+                double scale = (event.getDeltaY() < 0)
+                    ? Math.max(map.getScaleX() - 0.1, MIN_SCALE)
+                    : Math.min(map.getScaleX() + 0.1, MAX_SCALE);
+                map.setScaleX(scale);
+                map.setScaleY(scale);
+
+                event.consume();
             }
         }
     //#endregion
 
-    protected void onHoverNode(MapLocation mapLocation) {
-        mapLocation.node.setStroke(Color.GRAY);
-        mapLocation.node.setStrokeWidth(5f);
-    }
+    //#region Node Interaction Feedback
+        protected void onHoverNode(MapLocation mapLocation) {
+            mapLocation.node.setStroke(Color.GRAY);
+            mapLocation.node.setStrokeWidth(5f);
+        }
 
-    protected void offHoverNode(MapLocation mapLocation) {
-        mapLocation.node.setStroke(Color.DARKSLATEGREY);
-        mapLocation.node.setStrokeWidth(1f);
-    }
+        protected void offHoverNode(MapLocation mapLocation) {
+            mapLocation.node.setStroke(Color.DARKSLATEGREY);
+            mapLocation.node.setStrokeWidth(1f);
+        }
 
-    protected void onActiveNode(MapLocation mapLocation) {
-        mapLocation.node.setFill(Color.BLACK);
-    }
+        protected void onActiveNode(MapLocation mapLocation) {
+            mapLocation.node.setFill(Color.BLACK);
+        }
 
-    protected void offActiveNode(MapLocation mapLocation) {
-        mapLocation.node.setFill(Color.DARKCYAN);
-    }
+        protected void offActiveNode(MapLocation mapLocation) {
+            mapLocation.node.setFill(Color.DARKCYAN);
+        }
+    //#endregion
 
     //#region Utility
         private Circle drawCircle(double x, double y) {
