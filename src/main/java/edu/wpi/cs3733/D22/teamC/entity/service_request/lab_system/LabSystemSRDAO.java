f8c481d4d9a0a86017a4dd1cece52b60a8cd95a9
Brian <brianshin100@yahoo.com>
2022-04-03 19:29:19 -0400

    Implement Lab System SR DB and DAO

--- src/main/java/edu/wpi/cs3733/D22/teamC/entity/service_request/lab_system/LabSystemSRDAO.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/entity/service_request/lab_system/LabSystemSRDAO.java
@@ -1,31 +1,31 @@
-package edu.wpi.cs3733.D22.teamC.entity.service_request.medical_equipment;
+package edu.wpi.cs3733.D22.teamC.entity.service_request.lab_system;
 
 import edu.wpi.cs3733.D22.teamC.entity.service_request.ServiceRequestDAO;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public abstract class MedicalEquipmentSRDAO extends ServiceRequestDAO<MedicalEquipmentSR> {
+public abstract class LabSystemSRDAO extends ServiceRequestDAO<LabSystemSR> {
     /**
      * Modify ServiceRequest object from query resultSet.
      *
      * @param resultSet ResultSet from query to Service Request DB Table.
      * @return ServiceRequest object.
      */
-    protected MedicalEquipmentSR modifyServiceRequest(ResultSet resultSet, MedicalEquipmentSR serviceRequest) {
+    protected LabSystemSR modifyServiceRequest(ResultSet resultSet, LabSystemSR serviceRequest) {
         serviceRequest = super.modifyServiceRequest(resultSet, serviceRequest);
         try {
             // Create generic SR then convert and modify into MedicalEquipmentServiceRequest
             serviceRequest.setRequestID(resultSet.getInt("ID")); // redundant?
-            serviceRequest.setEquipmentID(typesafeTrim(resultSet.getString("EQUIPID")));
-            serviceRequest.setEquipmentType(MedicalEquipmentSR.EquipmentType.valueOf(resultSet.getString("EQUIPTYPE")));
-
+            serviceRequest.setLabType(LabSystemSR.LabType.valueOf(resultSet.getString("labType")));
+            serviceRequest.setPatientID(typesafeTrim(resultSet.getString("patientID")));
+    
             return serviceRequest;
-
+    
         } catch (SQLException e) {
             System.out.println("Creation of object from database ResultSet failed.");
             e.printStackTrace();
-
+    
             return null;
         }
     }
