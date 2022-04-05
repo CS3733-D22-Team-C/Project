102b76bf8579b122c1d4d55d35d2e5ee13fd45c3
Brandon Vuong <vuong.dbrandon@gmail.com>
2022-04-04 23:52:30 -0400

    Create LocationInfoPaneController.java
    - Add function to set editability of all text fields
    
    - Add function to set visibility for entirety of the location box

---
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/location/map/LocationInfoPaneController.java
@@ -0,0 +1,46 @@
+package edu.wpi.cs3733.D22.teamC.controller.location.map;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LocationInfoPaneController implements Initializable {
+
+    @FXML
+    TextField IDTextField;
+    @FXML
+    TextField LongNameTextField;
+    @FXML
+    TextField ShortNameTextField;
+    @FXML
+    TextField TypeTextField;
+    @FXML
+    VBox LocationVBox;
+
+    // Parent Controller
+    BaseMapViewController baseMapViewController;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void setBaseMapViewController(BaseMapViewController baseMapViewController) {
+        this.baseMapViewController = baseMapViewController;
+    }
+
+    public void setEditable(boolean editable) {
+        IDTextField.setEditable(editable);
+        LongNameTextField.setEditable(editable);
+        ShortNameTextField.setEditable(editable);
+        TypeTextField.setEditable(editable);
+    }
+
+    public void setVisible(boolean visible) {
+        LocationVBox.setVisible(visible);
+    }
+}
