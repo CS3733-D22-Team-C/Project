ca4bea676a4d30cbb23c054978a91cd781305671
njpires20 <nelsonpires0202@gmail.com>
2022-04-04 20:58:20 -0400

    Update MedicalEquipmentSRResolveController.java

--- src/main/java/edu/wpi/cs3733/D22/teamC/controller/service_request/medical_equipment/MedicalEquipmentSRResolveController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/service_request/medical_equipment/MedicalEquipmentSRResolveController.java
@@ -3,6 +3,7 @@ package edu.wpi.cs3733.D22.teamC.controller.service_request.medical_equipment;
 import com.jfoenix.controls.JFXComboBox;
 import edu.wpi.cs3733.D22.teamC.App;
 import edu.wpi.cs3733.D22.teamC.controller.general.ServiceRequestResolveController;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.ServiceRequest;
 import edu.wpi.cs3733.D22.teamC.entity.service_request.medical_equipment.MedicalEquipmentSR;
 import edu.wpi.cs3733.D22.teamC.entity.service_request.medical_equipment.MedicalEquipmentSRDAOImpl;
 import javafx.event.ActionEvent;
@@ -32,7 +33,7 @@ public class MedicalEquipmentSRResolveController extends ServiceRequestResolveCo
         MedicalEquipmentSR medicalEquipmentSR = medicalEquipmentSRDAOImpl.getServiceRequest(Integer.parseInt(requestID.getText()));
 
         //Setting fields from Querying Database
-        equipmentType.setPromptText(medicalEquipmentSR.getEquipmentType());
+        equipmentType.setPromptText(medicalEquipmentSR.getEquipmentType().toString());
         equipmentID.setPromptText(medicalEquipmentSR.getEquipmentID());
         description.setText(medicalEquipmentSR.getDescription());
         creationTime.setText(medicalEquipmentSR.getCreationTimestamp().toString());
@@ -50,7 +51,7 @@ public class MedicalEquipmentSRResolveController extends ServiceRequestResolveCo
         MedicalEquipmentSR medicalEquipmentSR = medicalEquipmentSRDAOImpl.getServiceRequest(Integer.parseInt(requestID.getText()));
 
         //Updating to Done in Database
-        medicalEquipmentSR.setStatus("Done"); //TODO Should be an enum
+        medicalEquipmentSR.setStatus(ServiceRequest.Status.Done); //TODO Should be an enum
         medicalEquipmentSR.setDescription(description.getText());
         medicalEquipmentSRDAOImpl.updateServiceRequest(medicalEquipmentSR);
 
