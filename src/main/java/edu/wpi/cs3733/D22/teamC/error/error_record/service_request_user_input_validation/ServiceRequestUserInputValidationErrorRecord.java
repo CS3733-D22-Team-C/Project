1a6a0d1f4601d3b15800640b1d4648cbd265b04b
Matthew McAlarney <67719286+MacCode7110@users.noreply.github.com>
2022-04-05 02:00:11 -0400

    ChangedNamesOfAFewErrors

--- src/main/java/edu/wpi/cs3733/D22/teamC/error/error_record/service_request_user_input_validation/ServiceRequestUserInputValidationErrorRecord.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/error/error_record/service_request_user_input_validation/ServiceRequestUserInputValidationErrorRecord.java
@@ -10,16 +10,16 @@ public class ServiceRequestUserInputValidationErrorRecord {
             new ServiceRequestUserInputValidationErrorItem(4, "Required field Priority is missing"),
             new ServiceRequestUserInputValidationErrorItem(5, "Assignee ID does not exist"),
             new ServiceRequestUserInputValidationErrorItem(6, "Location ID does not exist"),
-            new ServiceRequestUserInputValidationErrorItem(7, "Maintenance Type needs to be filled"),
-            new ServiceRequestUserInputValidationErrorItem(8, "Lab Type needs to be filled"),
-            new ServiceRequestUserInputValidationErrorItem(9, "Patient ID needs to be filled"),
-            new ServiceRequestUserInputValidationErrorItem(10, "Equipment Type needs to be filled"),
-            new ServiceRequestUserInputValidationErrorItem(11, "Equipment ID needs to be filled"),
-            new ServiceRequestUserInputValidationErrorItem(12, "Patient ID needs to be filled"),
-            new ServiceRequestUserInputValidationErrorItem(13, "Medicine needs to be filled"),
-            new ServiceRequestUserInputValidationErrorItem(14, "Dosage needs to be filled"),
-            new ServiceRequestUserInputValidationErrorItem(15, "Sanitation Type needs to be filled"),
-            new ServiceRequestUserInputValidationErrorItem(16, "Security Type needs to be filled")
+            new ServiceRequestUserInputValidationErrorItem(7, "Required field Maintenance Type needs to be filled"),
+            new ServiceRequestUserInputValidationErrorItem(8, "Required field Lab Type needs to be filled"),
+            new ServiceRequestUserInputValidationErrorItem(9, "Required field Patient ID needs to be filled"),
+            new ServiceRequestUserInputValidationErrorItem(10, "Required field Equipment Type needs to be filled"),
+            new ServiceRequestUserInputValidationErrorItem(11, "Required field Equipment ID needs to be filled"),
+            new ServiceRequestUserInputValidationErrorItem(12, "Required field Patient ID needs to be filled"),
+            new ServiceRequestUserInputValidationErrorItem(13, "Required field Medicine needs to be filled"),
+            new ServiceRequestUserInputValidationErrorItem(14, "Required field Dosage needs to be filled"),
+            new ServiceRequestUserInputValidationErrorItem(15, "Required field Sanitation Type needs to be filled"),
+            new ServiceRequestUserInputValidationErrorItem(16, "Required field Security Type needs to be filled")
 };
 
 }
