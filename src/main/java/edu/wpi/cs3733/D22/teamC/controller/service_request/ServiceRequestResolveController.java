4e7cfc62327912d67eb9e8818d762c12a48521e4
njpires20 <100165903+njpires20@users.noreply.github.com>
2022-04-05 22:14:34 -0400

    TC-132: Lab System Resolve Page (#59)
    * Started creation of Lab System SR Resolve
    
    - FXML file, and ResolveController created
    - Create Controller updated
    
    * Fixed Text Field Error and lab dropdown
    
    * Update ServiceRequestResolveController.java
    
    * Fixed Bugs
    
    Co-authored-by: mmueting <70529094+mmueting@users.noreply.github.com>

--- src/main/java/edu/wpi/cs3733/D22/teamC/controller/service_request/ServiceRequestResolveController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/service_request/ServiceRequestResolveController.java
@@ -118,7 +118,6 @@ public class ServiceRequestResolveController {
             {
                serviceRequest.setLocation(hospitalLocation.getValue());
             }
-
             //Status
         }
         else {
