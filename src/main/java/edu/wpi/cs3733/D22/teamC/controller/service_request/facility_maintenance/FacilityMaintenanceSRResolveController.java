beec159a24172387f37db756f8d3ca6bc6aafec3
Matthew McAlarney <67719286+MacCode7110@users.noreply.github.com>
2022-04-05 23:14:00 -0400

    CodeFrameworkComplete

--- src/main/java/edu/wpi/cs3733/D22/teamC/controller/service_request/facility_maintenance/FacilityMaintenanceSRResolveController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/service_request/facility_maintenance/FacilityMaintenanceSRResolveController.java
@@ -23,12 +23,70 @@ public class FacilityMaintenanceSRResolveController extends ServiceRequestResolv
     public void setup(ServiceRequest serviceRequest, boolean isEditMode) {
         super.setup(serviceRequest, isEditMode);
         //Need the FacilityMaintenanceDAOImpl to complete this function
+
+        /*
+        // Querying for full Medical Equipment Service Request
+        MedicalEquipmentSRDAOImpl medicalEquipmentSRDAOImpl = new MedicalEquipmentSRDAOImpl();
+        MedicalEquipmentSR medicalEquipmentSR = medicalEquipmentSRDAOImpl.getServiceRequest(serviceRequest.getRequestID());
+
+        // Setting fields from Querying Database
+        equipmentType.setPromptText(medicalEquipmentSR.getEquipmentType().toString());
+        equipmentID.setPromptText(medicalEquipmentSR.getEquipmentID());
+        description.setText(medicalEquipmentSR.getDescription());
+        creationTime.setText(medicalEquipmentSR.getCreationTimestamp().toString());
+
+        equipmentID.setDisable(true);
+        equipmentType.setDisable(true);
+         */
+
+        if(isEditMode){
+            // Equipment Type Dropdown
+            for (MedicalEquipmentSR.EquipmentType type : MedicalEquipmentSR.EquipmentType.values()) {
+                maintType.getItems().add(type.toString());
+            }
+            maintType.valueProperty().setValue(maintType.getPromptText());
+
+            //Status labels at bottom
+            if (requiredFieldsPresent()) secondStatus.setText("processing");
+            else secondStatus.setText("blank");
+
+            maintType.setDisable(false);
+        }
+        System.out.println(isEditMode);
     }
 
     @Override
     public void clickConfirm(ActionEvent event) {
         //Need access to the FacilityMaintenanceDAOImpl to complete this function
         super.clickConfirm(event);
+
+        /*
+        //Accessing Service Request in Database
+        MedicalEquipmentSRDAOImpl medicalEquipmentSRDAOImpl = new MedicalEquipmentSRDAOImpl();
+        MedicalEquipmentSR medicalEquipmentSR = medicalEquipmentSRDAOImpl.getServiceRequest(serviceRequest.getRequestID());
+        if(isEditMode){
+            //check if value has changed
+            if(equipmentID.getValue() != null)
+            {
+                medicalEquipmentSR.setEquipmentID(equipmentID.getValue());
+            }
+            if(equipmentType.getValue() != null) {
+                medicalEquipmentSR.setEquipmentType(MedicalEquipmentSR.EquipmentType.valueOf(equipmentType.getValue()));
+            }
+
+            System.out.println(requiredFieldsPresent());
+            //Status
+            if(requiredFieldsPresent())
+                medicalEquipmentSR.setStatus(ServiceRequest.Status.Processing);
+            else
+                medicalEquipmentSR.setStatus(ServiceRequest.Status.Blank);
+            medicalEquipmentSRDAOImpl.updateServiceRequest(medicalEquipmentSR);
+
+        }
+
+        //Back to service request view
+        App.instance.setView(App.VIEW_SERVICE_REQUESTS_PATH);
+         */
     }
 
     @Override
