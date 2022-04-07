45dd859232fd964351cb1d4cce056766c33d789a
JohnMuirhead000 <73247467+JohnMuirhead000@users.noreply.github.com>
2022-04-07 14:21:18 -0400

    insert and controller for security and medicine_delivery

---
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/service_request/medicine_delivery/MedicineDeliverySRInsertCreateController.java
@@ -0,0 +1,56 @@
+package edu.wpi.cs3733.D22.teamC.controller.service_request.medicine_delivery;
+
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTreeTableView;
+import edu.wpi.cs3733.D22.teamC.controller.service_request.InsertServiceRequestCreateController;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.ServiceRequestDAO;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.medicine_delivery.MedicineDeliverySR;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.medicine_delivery.MedicineDeliverySRDAO;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.medicine_delivery.MedicineDeliverySRDAOImpl;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.security.SecuritySR;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.security.SecuritySRDAOImpl;
+import edu.wpi.cs3733.D22.teamC.models.service_request.ServiceRequestTableDisplay;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MedicineDeliverySRInsertCreateController implements InsertServiceRequestCreateController<MedicineDeliverySR>, Initializable {
+    @FXML private TextField medicine;
+    @FXML private TextField dosage;
+    @FXML private TextField patientID;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    @Override
+    public void clearFields() {
+        medicine.setText(null);
+        dosage.setText(null);
+        patientID.setText(null);
+    }
+
+    @Override
+    public MedicineDeliverySR createServiceRequest() {
+        MedicineDeliverySR medicineDeliverySR = new MedicineDeliverySR();
+
+        medicineDeliverySR.setMedicine(medicine.getText());
+        medicineDeliverySR.setMedicine(dosage.getText());
+        medicineDeliverySR.setMedicine(patientID.getText());
+
+        return medicineDeliverySR;
+    }
+
+    @Override
+    public ServiceRequestDAO<MedicineDeliverySR> createServiceRequestDAO() {
+        return new MedicineDeliverySRDAOImpl();
+    }
+
+    @Override
+    public ServiceRequestTableDisplay<MedicineDeliverySR> setupTable(JFXTreeTableView<?> table) {
+        return new ServiceRequestTableDisplay(table);
+    }
+}
---
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/service_request/security/SecuritySRInsertCreateController.java
@@ -0,0 +1,51 @@
+package edu.wpi.cs3733.D22.teamC.controller.service_request.security;
+
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTreeTableView;
+import edu.wpi.cs3733.D22.teamC.controller.service_request.InsertServiceRequestCreateController;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.ServiceRequestDAO;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.security.SecuritySR;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.security.SecuritySRDAOImpl;
+import edu.wpi.cs3733.D22.teamC.models.service_request.ServiceRequestTableDisplay;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SecuritySRInsertCreateController implements InsertServiceRequestCreateController<SecuritySR>, Initializable {
+    @FXML
+    private JFXComboBox<String> securityType;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Priority dropdown
+        for (SecuritySR.SecurityType secType : SecuritySR.SecurityType.values()) {
+            securityType.getItems().add(secType.toString());
+        }
+    }
+
+    @Override
+    public void clearFields() {
+        securityType.valueProperty().setValue(null);
+    }
+
+    @Override
+    public SecuritySR createServiceRequest() {
+        SecuritySR sanitationSR = new SecuritySR();
+
+        sanitationSR.setSecurityType(SecuritySR.SecurityType.valueOf(securityType.getValue()));
+
+        return sanitationSR;
+    }
+
+    @Override
+    public ServiceRequestDAO<SecuritySR> createServiceRequestDAO() {
+        return new SecuritySRDAOImpl();
+    }
+
+    @Override
+    public ServiceRequestTableDisplay<SecuritySR> setupTable(JFXTreeTableView<?> table) {
+        return new ServiceRequestTableDisplay(table);
+    }
+}
