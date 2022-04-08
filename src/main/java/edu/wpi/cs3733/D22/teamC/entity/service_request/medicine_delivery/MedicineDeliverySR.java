4d4f111faeb813aa19cd41d6b3de829a2bd74e67
Brian Shin <brianshin100@yahoo.com>
2022-04-08 02:22:06 -0400

    Hibernation MedicineDeliverySR (#78)
    Deleted the MedicineDeliverySRDAOImpl and made the changes according to the errors caused.
    
    Changed the MDSRDAO and added tags to the MDSR.
    
    Co-authored-by: vshp22 <vpdendukuri@gmail.com>

--- src/main/java/edu/wpi/cs3733/D22/teamC/entity/service_request/medicine_delivery/MedicineDeliverySR.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/entity/service_request/medicine_delivery/MedicineDeliverySR.java
@@ -2,10 +2,22 @@ package edu.wpi.cs3733.D22.teamC.entity.service_request.medicine_delivery;
 
 import edu.wpi.cs3733.D22.teamC.entity.service_request.ServiceRequest;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "MEDICINE_DELIVERY_SR")
 public class MedicineDeliverySR extends ServiceRequest {
 
+
+    @Column (name = "Medicine")
     protected String medicine;
+
+    @Column (name = "Dosage")
     protected String dosage;
+
+    @Column (name = "PatientID")
     protected String patientID;     // TODO: Link to Patient
 
     public String getMedicine() {
