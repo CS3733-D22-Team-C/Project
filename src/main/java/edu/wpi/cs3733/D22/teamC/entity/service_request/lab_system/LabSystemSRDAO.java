7d4d9deb6a477d7d7b08f8c263d430fd7053519e
Brian Shin <brianshin100@yahoo.com>
2022-04-08 02:17:18 -0400

    Hibernated Lab Service Request files (#75)
    Co-authored-by: goreilly2001 <goreilly@wpi.edu>

--- src/main/java/edu/wpi/cs3733/D22/teamC/entity/service_request/lab_system/LabSystemSRDAO.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/entity/service_request/lab_system/LabSystemSRDAO.java
@@ -4,30 +4,37 @@ import edu.wpi.cs3733.D22.teamC.entity.service_request.ServiceRequestDAO;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import edu.wpi.cs3733.D22.teamC.entity.generic.DAO;
 
-public abstract class LabSystemSRDAO extends ServiceRequestDAO<LabSystemSR> {
-    /**
-     * Modify ServiceRequest object from query resultSet.
-     *
-     * @param resultSet ResultSet from query to Service Request DB Table.
-     * @return ServiceRequest object.
-     */
-    protected LabSystemSR modifyServiceRequest(ResultSet resultSet, LabSystemSR serviceRequest) {
-        // Modify ServiceRequest attributes
-        serviceRequest = super.modifyServiceRequest(resultSet, serviceRequest);
-
-        try {
-            // Modify LabSystemSR attributes
-            serviceRequest.setLabType(LabSystemSR.LabType.valueOf(typesafeTrim((resultSet.getString("labType")))));
-            serviceRequest.setPatientID(typesafeTrim(resultSet.getString("patientID")));
-    
-            return serviceRequest;
-
-        } catch (SQLException e) {
-            System.out.println("Creation of object from database ResultSet failed.");
-            e.printStackTrace();
-    
-            return null;
-        }
+public class LabSystemSRDAO extends DAO<LabSystemSR> {
+    @Override
+    protected Class<LabSystemSR> classType() {
+        return LabSystemSR.class;
     }
+
+
+//    /**
+//     * Modify ServiceRequest object from query resultSet.
+//     *
+//     * @param resultSet ResultSet from query to Service Request DB Table.
+//     * @return ServiceRequest object.
+//     */
+//    protected LabSystemSR modifyServiceRequest(ResultSet resultSet, LabSystemSR serviceRequest) {
+//        // Modify ServiceRequest attributes
+//        serviceRequest = super.modifyServiceRequest(resultSet, serviceRequest);
+//
+//        try {
+//            // Modify LabSystemSR attributes
+//            serviceRequest.setLabType(LabSystemSR.LabType.valueOf(typesafeTrim((resultSet.getString("labType")))));
+//            serviceRequest.setPatientID(typesafeTrim(resultSet.getString("patientID")));
+//
+//            return serviceRequest;
+//
+//        } catch (SQLException e) {
+//            System.out.println("Creation of object from database ResultSet failed.");
+//            e.printStackTrace();
+//
+//            return null;
+//        }
+//    }
 }
