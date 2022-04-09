ae24d4cfa0bc1d5bc58571a9e0a3e63d544eea2a
mmueting <70529094+mmueting@users.noreply.github.com>
2022-04-09 00:24:05 -0400

    Eye put on top of button

--- src/main/java/edu/wpi/cs3733/D22/teamC/App.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/App.java
@@ -105,7 +105,7 @@ public class App extends Application {
         
         stage.setFullScreen(true);
         
-        setView(HOME_PATH);
+        setView("view/service_request/service_request_landing_page.fxml");
     }
 
     @Override
