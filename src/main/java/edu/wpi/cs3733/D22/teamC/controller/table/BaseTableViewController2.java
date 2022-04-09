40ab3563be04734098f66c9468593ffb9ed24809
JohnMuirhead000 <73247467+JohnMuirhead000@users.noreply.github.com>
2022-04-09 16:34:34 -0400

    started on controllers; it now can update CSV and put table info into insert

--- src/main/java/edu/wpi/cs3733/D22/teamC/controller/table/BaseTableViewController2.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/table/BaseTableViewController2.java
@@ -1,35 +1,134 @@
 package edu.wpi.cs3733.D22.teamC.controller.table;
 
+import com.jfoenix.controls.JFXTreeTableView;
 import edu.wpi.cs3733.D22.teamC.App;
+import edu.wpi.cs3733.D22.teamC.controller.location.LocationsViewController;
+import edu.wpi.cs3733.D22.teamC.entity.location.Location;
+import edu.wpi.cs3733.D22.teamC.entity.location.LocationDAO;
+import edu.wpi.cs3733.D22.teamC.entity.location.LocationDAOImpl;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.ServiceRequest;
+import edu.wpi.cs3733.D22.teamC.models.generic.TableDisplay;
+import edu.wpi.cs3733.D22.teamC.models.location.LocationTableDisplay;
+import edu.wpi.cs3733.D22.teamC.models.service_request.ServiceRequestTableDisplay;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeTableRow;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
-public class BaseTableViewController2 implements Initializable {
+public class BaseTableViewController2<T extends Object> implements Initializable {
 
     @FXML private VBox insertBox;
-    private String PATH= "view/Table/Locations/table_insert.fxml";
     InserTableViewController insertController;
+
+    @FXML private TextField row5;
+
+    private Location currentLocation;
+
+
+
+    @FXML private JFXTreeTableView table;
+    private LocationTableDisplay tableDisplay;
+
+    //TODO abstract that this (same as above setUp)
+    private LocationsViewController viewController;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        //currentLocation = new Location();
         System.out.println("hello1");
-        this.setUp();
+
+        setUp("view/Table/Locations/table_insert.fxml");
+        rowInteraction();
     }
 
-    public void setUp(){
+    //TODO make the controller abstrsct (more than just location)
+    public void setUp(String insertPath){
+        //Will be a parameter
+        //LocationsViewController viewController
+        //insertController = new InserTableViewController();
         insertController = new InserTableViewController();
-        System.out.println("hello2");
-        setInsert(PATH);
+        setInsert(insertPath);
+        //step 1 get path of the specified insert, put it into place
+        //this.viewController  =  LocationsViewController();
+
+
+        //TODO add setUP function to viewController
+        //this.viewController.setUp(table);
+
+        //TODO to be abstracred l8ter
+        resetTableView();
+
+
+
 
     }
 
     public void setInsert(String path){
         App.View<InserTableViewController> view = App.instance.loadView(path);
-        //insertController = view.getController();
+        insertController = view.getController();
         insertBox.getChildren().add(view.getNode());
     }
+
+    //TODO
+    public void rowInteraction(){
+
+        //TODO figure out how to interact with certina row
+        table.setRowFactory(tv -> {
+            TreeTableRow<LocationTableDisplay.LocationTableEntry> row = new TreeTableRow<LocationTableDisplay.LocationTableEntry>();
+            row.setOnMouseClicked(event -> {
+                if (!row.isEmpty() && event.getButton() == MouseButton.PRIMARY) {
+
+                    if (event.getClickCount() == 2) {
+                        System.out.println(row.getText());
+
+                        //TODO insert controllr is not updating
+                        //insertController.setRows("test1", "test 2", "test3", "test 4");
+
+                        currentLocation = row.getItem().object;
+                        //currentLocation = location;
+
+                        currentLocation.setLongName("TESTSTS");
+                        LocationDAO locationDAO = new LocationDAOImpl();
+                        locationDAO.updateLocation(currentLocation);
+
+                        resetTableView();
+
+                        //fills the insert with info relevant to selected ROW
+                        insertController.getRowInfo(currentLocation);
+                    }
+                }
+            });
+            return row ;
+        });
+    }
+
+    public void backButton(){
+
+    }
+
+    private void resetTableView(){
+        tableDisplay = new LocationTableDisplay(table);
+
+        // Query Database
+        LocationDAO locationsDAO = new LocationDAOImpl();
+        List<Location> locations = locationsDAO.getAllLocations();
+
+        for(Location location2 : locations){
+            tableDisplay.addObject(location2);
+        }
+    }
+
+
+
+
+
+
+
 }
--- src/main/java/edu/wpi/cs3733/D22/teamC/controller/table/InserTableViewController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/table/InserTableViewController.java
@@ -2,9 +2,11 @@ package edu.wpi.cs3733.D22.teamC.controller.table;
 
 import com.jfoenix.controls.JFXButton;
 import edu.wpi.cs3733.D22.teamC.controller.service_request.BaseServiceRequestResolveController;
+import edu.wpi.cs3733.D22.teamC.entity.location.Location;
 import edu.wpi.cs3733.D22.teamC.entity.service_request.medicine_delivery.MedicineDeliverySR;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
@@ -14,12 +16,62 @@ public class InserTableViewController implements Initializable {
 
     @FXML private JFXButton submit;
 
+    @FXML
+    private TextField row1;
+
+    @FXML
+    private TextField row2;
+
+    @FXML
+    private TextField row3;
+    @FXML
+    private TextField row4;
+
+    @FXML
+    Label label1;
+    @FXML
+    Label label2;
+    @FXML
+    Label label3;
+    @FXML
+    Label label4;
+
+
+
     public void initialize(URL location, ResourceBundle resources) {
+
+        row1.setText("TEST TEST");
 
     }
 
+    public String getPath(){
+
+        //TODO write funciton to grab path of insert
+         return "view/Table/Locations/table_insert.fxml";
+
+    }
+
+    public void setRows(String r1,String r2, String r3, String r4) {
+        System.out.println("HERE");
+        row1.setText(r1);
+        row2.setText(r2);
+        row3.setText(r3);
+        row4.setText(r4);
+    }
+
+
     public void setup()
     {
+    }
+
+    public void getRowInfo(Location location)
+    {
+        label1.setText("Location Name");
+        label2.setText("X Value");
+        label3.setText("Y Value");
+        label4.setText("Buiilding");
+
+        setRows(location.getLongName(), Integer.toString(location.getX()), Integer.toString(location.getY()), location.getBuilding());
     }
 
 
