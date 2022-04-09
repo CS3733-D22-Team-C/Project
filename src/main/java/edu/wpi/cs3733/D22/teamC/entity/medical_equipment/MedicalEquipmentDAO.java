0d7f95f8c0527cde4f9947da1d98864478ae4688
Brian Shin <brianshin100@yahoo.com>
2022-04-09 19:26:42 -0400

    TC-151: Medical Equipment DAO (#90)

---
+++ src/main/java/edu/wpi/cs3733/D22/teamC/entity/medical_equipment/MedicalEquipmentDAO.java
@@ -0,0 +1,23 @@
+package edu.wpi.cs3733.D22.teamC.entity.medical_equipment;
+
+import edu.wpi.cs3733.D22.teamC.HibernateManager;
+import edu.wpi.cs3733.D22.teamC.entity.generic.DAO;
+
+import java.util.List;
+
+public class MedicalEquipmentDAO extends DAO<MedicalEquipment> {
+
+    
+    protected Class<MedicalEquipment> classType() {
+        return MedicalEquipment.class;
+    }
+
+    /**
+     * finds the list of equipment at designated location
+     * @param locationID the idenitifer of the location
+     * @return the list of equipments at this location
+     */
+    public List<MedicalEquipment> getEquipmentByLocation(int locationID) {
+        return HibernateManager.filterQuery("from " + classType().getName() + " where LOCATIONID = " + locationID);
+    }
+}
