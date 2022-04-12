f9d92ad21d85f52eb972e100b2b555c79c6130be
vagmi20 <nagavagmi.bh@gmail.com>
2022-04-12 01:17:18 -0400

    added CSV reader/writer for medequip

---
+++ src/main/java/edu/wpi/cs3733/D22/teamC/fileio/csv/MedicalEquipmentCSVReader.java
@@ -0,0 +1,43 @@
+package edu.wpi.cs3733.D22.teamC.fileio.csv;
+
+import edu.wpi.cs3733.D22.teamC.entity.medical_equipment.MedicalEquipment;
+
+public class MedicalEquipmentCSVReader extends CSVReader<MedicalEquipment> {
+    /**
+     * Maps Medical Equipment (header, value) to a value to change for the object
+     * @param object The object to be modified.
+     * @param header The header to be mapped to an attribute.
+     * @param value The value for the current attribute.
+     * @return The medical equipment modified with the value at corresponding attribute
+     */
+    @Override
+    protected MedicalEquipment parseAttribute(MedicalEquipment object, String header, String value) {
+        switch(header) {
+            case "ID":
+                object.setEquipID(value);
+                break;
+            case "locationID":
+                object.setLocationID(value);
+                break;
+            case "typeNumber":
+                object.setTypeNumber(Integer.parseInt(value));
+                break;
+            case "equipType":
+                object.setEquipmentType(MedicalEquipment.EquipmentType.valueOf(value));
+                break;
+            case "equipStatus":
+                object.setStatus(MedicalEquipment.EquipmentStatus.valueOf(value));
+                break;
+        }
+        return object;
+    }
+
+    /**
+     * wrapper for medical equipment constructor
+     * @return Newly created medical equipment object
+     */
+    @Override
+    protected MedicalEquipment createObject() {
+        return new MedicalEquipment();
+    }
+}
