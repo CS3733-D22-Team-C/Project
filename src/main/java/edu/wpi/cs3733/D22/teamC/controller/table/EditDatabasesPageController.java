3cca5b0ec4cf09bc9ce5502c343e702e2676e041
mmueting <70529094+mmueting@users.noreply.github.com>
2022-04-13 02:24:09 -0400

    CSV component functional (#108)
    * CSV component functional
    
    Saves the files it chose and puts the name of the file into the text box
    
    * CSV Page Created
    
    - Can select a folder
    - Right now will print out the paths of what is in the folder
    
    * Functional CSV Page
    
    - Not connected to backend
    
    * Basic Outline of Edit Databases Page
    
    * Edit Database page
    
    * Functional Database Page
    
    * Bug fix
    
    * Stylizing database page
    
    * Update App.java

---
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/table/EditDatabasesPageController.java
@@ -0,0 +1,56 @@
+package edu.wpi.cs3733.D22.teamC.controller.table;
+
+import edu.wpi.cs3733.D22.teamC.App;
+import edu.wpi.cs3733.D22.teamC.controller.location.LocationsViewController;
+import edu.wpi.cs3733.D22.teamC.controller.location.map.CSVComponent;
+import edu.wpi.cs3733.D22.teamC.controller.service_request.InsertServiceRequestCreateController;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.ServiceRequest;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EditDatabasesPageController implements Initializable {
+
+    @FXML private VBox insertVbox;
+
+    CSVComponent insertController;
+
+    final static String TABLE_SKELETON = "view/table/base_view.fxml";
+    final static String TABLE_EMPLOYEE = "view/table/employee/table_insert.fxml";
+    final static String TABLE_LOCATION = "view/table/locations/table_insert.fxml";
+    final static String TABLE_MEDICAL_EQUIPMENT = "view/table/medical_equipment/table_insert.fxml";
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setInsert();
+    }
+    @FXML
+    void clickEmployee(ActionEvent event) {
+        App.instance.setSkeletonView(TABLE_SKELETON, TABLE_EMPLOYEE);
+
+    }
+
+    @FXML
+    void clickLocations(ActionEvent event) {
+        App.instance.setSkeletonView(TABLE_SKELETON, TABLE_LOCATION);
+    }
+
+    @FXML
+    void clickMedicalEquipment(ActionEvent event) {
+        App.instance.setSkeletonView(TABLE_SKELETON, TABLE_MEDICAL_EQUIPMENT);
+
+    }
+
+    public void setInsert() {
+        String viewFile = "view/location/map/controls/csv_page.fxml";
+
+        App.View<CSVComponent> view = App.instance.loadView(viewFile);
+        insertVbox.getChildren().add(view.getNode());
+        insertController = view.getController();
+    }
+
+}
