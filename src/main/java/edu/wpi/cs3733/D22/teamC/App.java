4ed1e92661aac8876bcc50d5748330b50a2f96ad
JohnMuirhead000 <73247467+JohnMuirhead000@users.noreply.github.com>
2022-04-12 22:26:29 -0400

    medical equiptment insert

--- src/main/java/edu/wpi/cs3733/D22/teamC/App.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/App.java
@@ -152,7 +152,7 @@ public class App extends Application {
         setViewStatic(LOGIN_PATH);
 
         // TODO: Hook up via sidebar
-        setSkeletonView("view/table/base_view.fxml", "view/table/employee/table_insert.fxml");
+        setSkeletonView("view/table/base_view.fxml", "view/table/medical_equipment/table_insert.fxml");
     }
 
     @Override
