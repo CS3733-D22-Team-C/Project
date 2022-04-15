0449c3981eac4fc1825f025b2a30152be4048df6
mmueting <70529094+mmueting@users.noreply.github.com>
2022-04-14 23:27:30 -0400

    Patient Popup Window Created

---
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/table/PatientTableViewInsertController.java
@@ -0,0 +1,23 @@
+package edu.wpi.cs3733.D22.teamC.controller.table;
+
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTreeTableView;
+import edu.wpi.cs3733.D22.teamC.entity.patient.Patient;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class PatientTableViewInsertController {
+
+        @FXML
+        private JFXButton selectButton;
+
+        @FXML
+        private JFXTreeTableView table;
+
+        @FXML
+        void clickSelect(ActionEvent event) {
+
+        }
+
+    }
