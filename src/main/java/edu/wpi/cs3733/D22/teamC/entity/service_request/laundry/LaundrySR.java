f1a94f0a4d0f98f502e8a5ac7f6432f08e68f0e6
goreilly2001 <goreilly@wpi.edu>
2022-04-14 22:09:50 -0400

    Laundry DAO corrected and completed

--- src/main/java/edu/wpi/cs3733/D22/teamC/entity/service_request/laundry/LaundrySR.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/entity/service_request/laundry/LaundrySR.java
@@ -2,6 +2,8 @@
 package edu.wpi.cs3733.D22.teamC.entity.service_request.laundry;
 
 import edu.wpi.cs3733.D22.teamC.entity.service_request.ServiceRequest;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.sanitation.SanitationSR;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.Id;
@@ -13,19 +15,46 @@ import javax.persistence.*;
 @Table(name = "LAUNDRY")
 public class LaundrySR extends ServiceRequest {
 
-    @Id
-    @Column(name = "washed")
-    private String washed;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "LaundryType")
+    private LaundryType laundryType;
 
-    @Column(name = "quantity")
-    private String firstName;
+    public enum LaundryType {
+        Bedding,
+        Towel,
+        Gown,
+        Blanket
+    }
 
-    public LaundrySR()
-    { super(); }
+    @Column(name = "Quantity")
+    private String quantity;
+
+    public LaundrySR() {}
 
     public LaundrySR(ServiceRequest serviceRequest)
     { super(serviceRequest); }
 
+    public LaundryType getLaundryType(){ return laundryType;}
+
+    public void setLaundryType(LaundryType laundryType) {
+        this.laundryType = laundryType;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        LaundrySR that = (LaundrySR) o;
+        return laundryType == that.laundryType;
+    }
 }
 
 
