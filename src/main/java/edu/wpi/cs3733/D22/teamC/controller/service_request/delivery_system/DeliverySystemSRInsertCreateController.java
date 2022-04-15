c268a72eab16f00d5801a354586b45b42fa1d270
Brian <brianshin100@yahoo.com>
2022-04-15 03:00:54 -0400

    Update patient popup

--- src/main/java/edu/wpi/cs3733/D22/teamC/controller/service_request/delivery_system/DeliverySystemSRInsertCreateController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/service_request/delivery_system/DeliverySystemSRInsertCreateController.java
@@ -1,25 +1,44 @@
 package edu.wpi.cs3733.D22.teamC.controller.service_request.delivery_system;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTreeTableView;
 import edu.wpi.cs3733.D22.teamC.controller.service_request.InsertServiceRequestCreateController;
 import edu.wpi.cs3733.D22.teamC.entity.generic.DAO;
+import edu.wpi.cs3733.D22.teamC.entity.patient.Patient;
+import edu.wpi.cs3733.D22.teamC.entity.patient.PatientDAO;
 import edu.wpi.cs3733.D22.teamC.entity.service_request.delivery_system.DeliverySystemSR;
 import edu.wpi.cs3733.D22.teamC.entity.service_request.delivery_system.DeliverySystemSRDAO;
+import edu.wpi.cs3733.D22.teamC.models.patient.PatientSelectorWindow;
 import edu.wpi.cs3733.D22.teamC.models.service_request.ServiceRequestTableDisplay;
 import edu.wpi.cs3733.D22.teamC.models.service_request.delivery_system.DeliverySystemSRTableDisplay;
+import edu.wpi.cs3733.D22.teamC.models.utils.ComponentWrapper;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TextField;
+import org.controlsfx.control.SearchableComboBox;
 
+import javafx.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DeliverySystemSRInsertCreateController implements InsertServiceRequestCreateController<DeliverySystemSR>, Initializable {
     @FXML
     private JFXComboBox<String> deliveryType;
+    
     @FXML
-    private TextField patientID;
+    private SearchableComboBox<Patient> patientComboBox;
+    
+    @FXML
+    private JFXButton patientTableButton;
+    
+    @FXML
+    public void goToPatientTable(ActionEvent event) {
+        new PatientSelectorWindow(patient -> this.setPatientComboBox(patient));
+    }
+    
+    public void setPatientComboBox(Patient patientComboBox) {
+        this.patientComboBox.setValue(patientComboBox);
+    }
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -27,12 +46,13 @@ public class DeliverySystemSRInsertCreateController implements InsertServiceRequ
         for (DeliverySystemSR.DeliveryType labs : DeliverySystemSR.DeliveryType.values()) {
             deliveryType.getItems().add(labs.toString());
         }
+        patientComboBox.getItems().setAll(new PatientDAO().getAll());
     }
     
     @Override
     public void clearFields() {
         deliveryType.valueProperty().setValue(null);
-        patientID.setText(null);
+        patientComboBox.setValue(null);
     }
     
     @Override
@@ -40,7 +60,7 @@ public class DeliverySystemSRInsertCreateController implements InsertServiceRequ
         DeliverySystemSR deliverySR = new DeliverySystemSR();
     
         deliverySR.setDeliveryType(DeliverySystemSR.DeliveryType.valueOf(deliveryType.getValue()));
-        deliverySR.setPatientID(patientID.getText());
+        deliverySR.setPatientID(patientComboBox.getValue().getID());
         
         return deliverySR;
     }
@@ -59,9 +79,7 @@ public class DeliverySystemSRInsertCreateController implements InsertServiceRequ
     public boolean requiredFieldsPresent(){
         if(deliveryType.getValue() == null)
             return false;
-        if(patientID.getText().equals(""))
-            return false;
-        return true;
+        return patientComboBox.getValue() != null;
     }
     
     public DeliverySystemSR createNewServiceRequest() {
