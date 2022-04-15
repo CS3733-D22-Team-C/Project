7887e282106e468efeee21bf697c7893018751f7
Brandon Vuong <vuong.dbrandon@gmail.com>
2022-04-14 22:03:21 -0400

    Repurpose original menu to title bar

---
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/component/title_bar/TitleBarController.java
@@ -0,0 +1,11 @@
+package edu.wpi.cs3733.D22.teamC.controller.component.title_bar;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class TitleBarController {
+    @FXML
+    Label titleLabel;
+
+
+}
