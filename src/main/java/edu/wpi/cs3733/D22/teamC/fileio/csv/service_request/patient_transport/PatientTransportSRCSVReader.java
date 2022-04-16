3129c5e2cd2596219c795e8319e390ddbb4b2f84
mmueting <70529094+mmueting@users.noreply.github.com>
2022-04-15 21:02:53 -0400

    CSV Reader and Writer for Patient Transport

--- src/main/java/edu/wpi/cs3733/D22/teamC/fileio/csv/service_request/patient_transport/PatientTransportSRCSVReader.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/fileio/csv/service_request/patient_transport/PatientTransportSRCSVReader.java
@@ -1,16 +1,78 @@
 package edu.wpi.cs3733.D22.teamC.fileio.csv.service_request.patient_transport;
 
+import edu.wpi.cs3733.D22.teamC.entity.employee.Employee;
+import edu.wpi.cs3733.D22.teamC.entity.employee.EmployeeDAO;
+import edu.wpi.cs3733.D22.teamC.entity.patient.Patient;
+import edu.wpi.cs3733.D22.teamC.entity.patient.PatientDAO;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.ServiceRequest;
 import edu.wpi.cs3733.D22.teamC.entity.service_request.patient_transport.PatientTransportSR;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.sanitation.SanitationSR;
 import edu.wpi.cs3733.D22.teamC.fileio.csv.CSVReader;
 
+import java.sql.Timestamp;
+
 public class PatientTransportSRCSVReader extends CSVReader<PatientTransportSR> {
+    private EmployeeDAO employeeDAO = new EmployeeDAO();
+    private PatientDAO patientDAO = new PatientDAO();
+    /**
+     * Maps ServiceRequest (header, value) pairs to a value to change for the object.
+     * @param serviceRequest The object to be modified.
+     * @param header The header to be mapped to an attribute.
+     * @param value The value for the current attribute.
+     * @return The Location modified with the value at the corresponding attribute.
+     */
     @Override
-    protected PatientTransportSR parseAttribute(PatientTransportSR object, String header, String value) {
-        return null;
+    protected PatientTransportSR parseAttribute(PatientTransportSR serviceRequest, String header, String value) {
+        switch(header) {
+            case "requestID":
+                serviceRequest.setID(value);
+                break;
+            case "creatorID":
+                Employee creator = employeeDAO.getByID(value);
+                serviceRequest.setCreator(creator);
+                break;
+            case "assigneeID":
+                Employee assignee = employeeDAO.getByID(value);
+                serviceRequest.setAssignee(assignee);
+                break;
+            case "location":
+                serviceRequest.setLocation(value);
+                break;
+            case "creationTimestamp":
+                serviceRequest.setCreationTimestamp(Timestamp.valueOf(value));
+
+                break;
+            case "status":
+                serviceRequest.setStatus(ServiceRequest.Status.valueOf(value));
+                break;
+            case "priority":
+                serviceRequest.setPriority(ServiceRequest.Priority.valueOf(value));
+                break;
+            case "requestType":
+                serviceRequest.setRequestType(ServiceRequest.RequestType.valueOf(value));
+                break;
+            case "description":
+                serviceRequest.setDescription(value);
+                break;
+            case "patientID":
+                serviceRequest.setPatientID(value);
+                break;
+            case "transportTime":
+                serviceRequest.setTransportTime(Timestamp.valueOf(value));
+                break;
+            case "modifierID":
+                Employee modifier = employeeDAO.getByID(value);
+                serviceRequest.setModifier(modifier);
+                break;
+            case "modifiedTimestamp":
+                serviceRequest.setModifiedTimestamp(Timestamp.valueOf(value)); //TODO: Handle the modified time
+                break;
+        }
+        return serviceRequest;
     }
 
     @Override
     protected PatientTransportSR createObject() {
-        return null;
+        return new PatientTransportSR();
     }
 }
--- src/main/java/edu/wpi/cs3733/D22/teamC/fileio/csv/service_request/patient_transport/PatientTransportSRCSVWriter.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/fileio/csv/service_request/patient_transport/PatientTransportSRCSVWriter.java
@@ -1,16 +1,78 @@
 package edu.wpi.cs3733.D22.teamC.fileio.csv.service_request.patient_transport;
 
+import edu.wpi.cs3733.D22.teamC.entity.employee.Employee;
 import edu.wpi.cs3733.D22.teamC.entity.service_request.patient_transport.PatientTransportSR;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.sanitation.SanitationSR;
 import edu.wpi.cs3733.D22.teamC.fileio.csv.CSVWriter;
 
 public class PatientTransportSRCSVWriter extends CSVWriter<PatientTransportSR> {
     @Override
     protected String[] compileHeaders() {
-        return new String[0];
+        return new String[]{
+                "requestID",
+                "creatorID",
+                "assigneeID",
+                "location",
+                "creationTimestamp",
+                "status",
+                "priority",
+                "requestType",
+                "description",
+                "patientID",
+                "transportTime",
+                "modifierID",
+                "modifiedTimestamp"
+        };
     }
 
     @Override
-    protected String compileAttribute(PatientTransportSR object, String header) {
-        return null;
+    protected String compileAttribute(PatientTransportSR serviceRequest, String header) {
+        String output = "";
+        switch (header) {
+            case "requestID":
+                output = serviceRequest.getID();
+                break;
+            case "creatorID":
+                Employee employee = serviceRequest.getCreator();
+                output = (employee != null) ? employee.getID() : "";
+                break;
+            case "assigneeID":
+                Employee assignee = serviceRequest.getAssignee();
+                output = (assignee != null) ? assignee.getID() : "";
+                break;
+            case "location":
+                output = serviceRequest.getLocation();
+                break;
+            case "creationTimestamp":
+                output = (serviceRequest.getCreationTimestamp() == null) ? "" : serviceRequest.getCreationTimestamp().toString();
+                break;
+            case "status":
+                output = serviceRequest.getStatus().toString();
+                break;
+            case "priority":
+                output = serviceRequest.getPriority().toString();
+                break;
+            case "requestType":
+                output = serviceRequest.getRequestType().toString();
+                break;
+            case "description":
+                output = serviceRequest.getDescription();
+                break;
+            case "patientID":
+                output = serviceRequest.getPatientID();
+                break;
+            case "transportTime":
+                output = serviceRequest.getTransportTime().toString();
+            case "modifierID":
+                Employee modifier = serviceRequest.getModifier();
+                output = (modifier != null) ? modifier.getID() : "";
+                break;
+            case "modifiedTimestamp":
+                output = (serviceRequest.getModifiedTimestamp() == null) ? "" : serviceRequest.getModifiedTimestamp().toString();
+                break;
+            default:
+                break;
+        }
+        return output;
     }
 }
