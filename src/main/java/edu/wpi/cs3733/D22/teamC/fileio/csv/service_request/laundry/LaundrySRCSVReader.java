bcf01a1d0fb87cfc04f2ed65a8bea6694a1f4f99
goreilly2001 <goreilly@wpi.edu>
2022-04-15 20:46:54 -0400

    added csv reader and writer

---
+++ src/main/java/edu/wpi/cs3733/D22/teamC/fileio/csv/service_request/laundry/LaundrySRCSVReader.java
@@ -0,0 +1,76 @@
+package edu.wpi.cs3733.D22.teamC.fileio.csv.service_request.laundry;
+
+import edu.wpi.cs3733.D22.teamC.entity.employee.Employee;
+import edu.wpi.cs3733.D22.teamC.entity.employee.EmployeeDAO;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.ServiceRequest;
+import edu.wpi.cs3733.D22.teamC.entity.service_request.laundry.LaundrySR;
+import edu.wpi.cs3733.D22.teamC.fileio.csv.CSVReader;
+
+import java.sql.Timestamp;
+
+public class LaundrySRCSVReader extends CSVReader<LaundrySR> {
+    private EmployeeDAO employeeDAO = new EmployeeDAO();
+        /**
+         * Maps ServiceRequest (header, value) pairs to a value to change for the object.
+         * @param serviceRequest The object to be modified.
+         * @param header The header to be mapped to an attribute.
+         * @param value The value for the current attribute.
+         * @return The Location modified with the value at the corresponding attribute.
+         */
+        @Override
+        protected LaundrySR parseAttribute(LaundrySR serviceRequest, String header, String value) {
+            switch(header) {
+                case "requestID":
+                    serviceRequest.setRequestID(value);
+                    break;
+                case "creatorID":
+                    Employee creator = employeeDAO.getByID(value);
+                    serviceRequest.setCreator(creator);
+                    break;
+                case "assigneeID":
+                    Employee assignee = employeeDAO.getByID(value);
+                    serviceRequest.setAssignee(assignee);
+                    break;
+                case "location":
+                    serviceRequest.setLocation(value);
+                    break;
+                case "creationTimestamp":
+                    serviceRequest.setCreationTimestamp(Timestamp.valueOf(value));
+
+                    break;
+                case "status":
+                    serviceRequest.setStatus(ServiceRequest.Status.valueOf(value));
+                    break;
+                case "priority":
+                    serviceRequest.setPriority(ServiceRequest.Priority.valueOf(value));
+                    break;
+                case "requestType":
+                    serviceRequest.setRequestType(ServiceRequest.RequestType.valueOf(value));
+                    break;
+                case "description":
+                    serviceRequest.setDescription(value);
+                    break;
+                case "laundryType":
+                    serviceRequest.setLaundryType(LaundrySR.LaundryType.valueOf(value));
+                    break;
+                case "quantity":
+                    serviceRequest.setQuantity(value);
+                    break;
+                case "modifierID":
+                    Employee modifier = employeeDAO.getByID(value);
+                    serviceRequest.setModifier(modifier);
+                    break;
+                case "modifiedTimestamp":
+                    serviceRequest.setModifiedTimestamp(Timestamp.valueOf(value)); //TODO: Handle the modified time
+                    break;
+            }
+            return serviceRequest;
+        }
+
+        @Override
+        protected LaundrySR createObject() {
+            return new LaundrySR();
+        }
+    }
+
+
