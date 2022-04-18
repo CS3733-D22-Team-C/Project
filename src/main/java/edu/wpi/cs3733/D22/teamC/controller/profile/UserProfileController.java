c1b011ada932c29ef394de5f63312e7dd4254245
JohnMuirhead000 <73247467+JohnMuirhead000@users.noreply.github.com>
2022-04-17 21:44:40 -0400

    working, just needs styling

---
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/profile/UserProfileController.java
@@ -0,0 +1,82 @@
+package edu.wpi.cs3733.D22.teamC.controller.profile;
+
+import edu.wpi.cs3733.D22.teamC.App;
+import edu.wpi.cs3733.D22.teamC.controller.service_request.InsertServiceRequestCreateController;
+import edu.wpi.cs3733.D22.teamC.entity.employee.Employee;
+import io.github.palexdev.materialfx.controls.MFXPasswordField;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UserProfileController implements Initializable {
+
+    @FXML Label name;
+    @FXML Label position;
+    @FXML Label contact;
+    @FXML Label address;
+    @FXML Button back;
+    @FXML Button changePassword;
+    @FXML VBox topNode;
+    @FXML Button submitChange;
+
+    @FXML MFXPasswordField beforePassword;
+    @FXML MFXPasswordField newPass;
+    @FXML MFXPasswordField newPassConfirm;
+
+
+    //ones made in the whatever
+    @FXML VBox mainNode;
+    @FXML VBox passwordNode;
+
+    Employee currentEmploy;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+//        App.View passWordPage = App.instance.loadView(PWnode);
+//        App.View mainNodePage = App.instance.loadView(Mnode);
+
+        currentEmploy = App.instance.getUserAccount();
+        name.setText(currentEmploy.getLastName() + " " + currentEmploy.getLastName());
+        position.setText(currentEmploy.getRole().toString());
+        contact.setText(currentEmploy.getAddress());
+        address.setText(currentEmploy.getAddress());
+        topNode.getChildren().remove(passwordNode);
+
+    }
+
+    public void backButton() {App.instance.setView(App.DASHBOARD_PATH);}
+
+    public void changePasswordAction(){
+        topNode.getChildren().remove(mainNode);
+        topNode.getChildren().add(passwordNode);
+    }
+
+    public void changePassword(){
+        if (beforePassword.getText().equals(currentEmploy.getPassword()) &&
+        newPass.getText().equals(newPassConfirm.getText()) && isLegal(newPass.getText())){
+            //set the new password
+            currentEmploy.setPassword(newPass.getText());
+            //Reset the fields
+            beforePassword.setText("");
+            newPass.setText("");
+            newPassConfirm.setText("");
+        }
+    }
+
+    public boolean isLegal(String password)
+    {
+        //The password paradox; the less popular a password is, the better it is.
+
+        //What other parameters do we need?
+        return password.length() <= 20;
+    }
+}
