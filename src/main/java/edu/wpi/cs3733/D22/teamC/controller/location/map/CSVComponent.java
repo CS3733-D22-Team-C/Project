079d43d3084c4593041f81bb00f3face1d811ad5
JohnMuirhead000 <73247467+JohnMuirhead000@users.noreply.github.com>
2022-04-17 23:55:40 -0400

    now it works on mac

--- src/main/java/edu/wpi/cs3733/D22/teamC/controller/location/map/CSVComponent.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/location/map/CSVComponent.java
@@ -114,12 +114,12 @@ public class CSVComponent {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         directoryChooser.setTitle("Export CSV File");
         File file = directoryChooser.showDialog(App.instance.getStage());
-        csvName.setText(":\\ ...\\" + file.getName());
+        csvName.setText("..."+ File.separator+ file.getName());
         savedFile = file;
 
         //Path to the files
         for (int i = 0; i < savedFile.list().length; i++) {
-            System.out.println(savedFile.getPath() + "\\" + savedFile.list()[i]);
+            System.out.println(savedFile.getPath() + File.separator + savedFile.list()[i]);
         }
     }
 
@@ -129,49 +129,49 @@ public class CSVComponent {
             if(floorsExport.isSelected()){
                 FloorDAO floorDAO = new FloorDAO();
                 List<Floor> floors = floorDAO.getAll();
-                CSVFacade.write(Floor.class, savedFile.getPath() + "\\" + FLOOR_CSV, floors);
+                CSVFacade.write(Floor.class, savedFile.getPath() + File.separator + FLOOR_CSV, floors);
             }
             if(locationsExport.isSelected()) {
                 LocationDAO locationDAO = new LocationDAO();
                 List<Location> locations = locationDAO.getAll();
-                CSVFacade.write(Location.class, savedFile.getPath() + "\\" + LOCATION_CSV, locations);
+                CSVFacade.write(Location.class, savedFile.getPath() + File.separator + LOCATION_CSV, locations);
             }
 
             //Service Request
             if(medicalEquipmentExport.isSelected()) {
                 MedicalEquipmentSRDAO medicalEquipmentSRDAO = new MedicalEquipmentSRDAO();
                 List<MedicalEquipmentSR> medicalEquipmentSRS = medicalEquipmentSRDAO.getAll();
-                CSVFacade.write(MedicalEquipmentSR.class, savedFile.getPath() + "\\" + MEDICAL_EQUIPMENT_CSV, medicalEquipmentSRS);
+                CSVFacade.write(MedicalEquipmentSR.class, savedFile.getPath() + File.separator + MEDICAL_EQUIPMENT_CSV, medicalEquipmentSRS);
             }
             if(medicineDeliveryExport.isSelected()) {
                 MedicineDeliverySRDAO medicineDeliverySRDAO = new MedicineDeliverySRDAO();
                 List<MedicineDeliverySR> medicineDeliverySRS = medicineDeliverySRDAO.getAll();
-                CSVFacade.write(MedicineDeliverySR.class, savedFile.getPath() + "\\" + MEDICINE_DELIVERY_CSV, medicineDeliverySRS);
+                CSVFacade.write(MedicineDeliverySR.class, savedFile.getPath() + File.separator + MEDICINE_DELIVERY_CSV, medicineDeliverySRS);
             }
             if(sanitationExport.isSelected()) {
                 SanitationSRDAO sanitationSRDAO = new SanitationSRDAO();
                 List<SanitationSR> sanitationSRS = sanitationSRDAO.getAll();
-                CSVFacade.write(SanitationSR.class, savedFile.getPath() + "\\" + SANITATION_CSV, sanitationSRS);
+                CSVFacade.write(SanitationSR.class, savedFile.getPath() + File.separator + SANITATION_CSV, sanitationSRS);
             }
             if(labSystemExport.isSelected()) {
                 LabSystemSRDAO labSystemSRDAO = new LabSystemSRDAO();
                 List<LabSystemSR> labSystemSRS = labSystemSRDAO.getAll();
-                CSVFacade.write(LabSystemSR.class, savedFile.getPath() + "\\" + LAB_SYSTEM_CSV, labSystemSRS);
+                CSVFacade.write(LabSystemSR.class, savedFile.getPath() + File.separator + LAB_SYSTEM_CSV, labSystemSRS);
             }
             if(facilityMaintenanceExport.isSelected()) {
                 FacilityMaintenanceSRDAO facilityMaintenanceSRDAO = new FacilityMaintenanceSRDAO();
                 List<FacilityMaintenanceSR> facilityMaintenanceSRS = facilityMaintenanceSRDAO.getAll();
-                CSVFacade.write(FacilityMaintenanceSR.class, savedFile.getPath() + "\\" + FACILITY_MAINTENANCE_CSV, facilityMaintenanceSRS);
+                CSVFacade.write(FacilityMaintenanceSR.class, savedFile.getPath() + File.separator + FACILITY_MAINTENANCE_CSV, facilityMaintenanceSRS);
             }
             if(securityExport.isSelected()) {
                 SecuritySRDAO securitySRDAO = new SecuritySRDAO();
                 List<SecuritySR> securitySRS = securitySRDAO.getAll();
-                CSVFacade.write(SecuritySR.class, savedFile.getPath() + "\\" + SECURITY_CSV, securitySRS);
+                CSVFacade.write(SecuritySR.class, savedFile.getPath() + File.separator + SECURITY_CSV, securitySRS);
             }
             if(deliveryExport.isSelected()) {
                 DeliverySystemSRDAO deliverySystemSRDAO = new DeliverySystemSRDAO();
                 List<DeliverySystemSR> deliverySystemSRS = deliverySystemSRDAO.getAll();
-                CSVFacade.write(DeliverySystemSR.class, savedFile.getPath() + "\\" + DELIVERY_SYSTEM_CSV, deliverySystemSRS);
+                CSVFacade.write(DeliverySystemSR.class, savedFile.getPath() + File.separator + DELIVERY_SYSTEM_CSV, deliverySystemSRS);
             }
 
 
@@ -179,32 +179,32 @@ public class CSVComponent {
             if(employeesExport.isSelected()) {
                 EmployeeDAO employeeDAO = new EmployeeDAO();
                 List<Employee> employees = employeeDAO.getAll();
-                CSVFacade.write(Employee.class, savedFile.getPath() + "\\" + EMPLOYEE_CSV, employees);
+                CSVFacade.write(Employee.class, savedFile.getPath() + File.separator + EMPLOYEE_CSV, employees);
             }
 
             if(patientExport.isSelected()) {
                 PatientDAO patientDAO =  new PatientDAO();
                 List<Patient> patients = patientDAO.getAll();
-                CSVFacade.write(Patient.class, savedFile.getPath() + "\\" + PATIENT_CSV, patients);
+                CSVFacade.write(Patient.class, savedFile.getPath() + File.separator + PATIENT_CSV, patients);
             }
 
             //Medical Equipment Entity
             if(medicalEquipmentEntityExport.isSelected()) {
                 MedicalEquipmentDAO medicalEquipmentDAO = new MedicalEquipmentDAO();
                 List<MedicalEquipment> medicalEquipments = medicalEquipmentDAO.getAll();
-                CSVFacade.write(MedicalEquipment.class, savedFile.getPath() + "\\" + MEDICAL_EQUIPMENT_ENTITY_CSV, medicalEquipments);
+                CSVFacade.write(MedicalEquipment.class, savedFile.getPath() + File.separator + MEDICAL_EQUIPMENT_ENTITY_CSV, medicalEquipments);
             }
         }
         else {
             //Map
             if(floorsImport.isSelected()){
-                List<Floor> floors = CSVFacade.read(Floor.class, savedFile.getPath() + "\\" + FLOOR_CSV);
+                List<Floor> floors = CSVFacade.read(Floor.class, savedFile.getPath() + File.separator + FLOOR_CSV);
                 FloorDAO floorDAO = new FloorDAO();
                 floorDAO.deleteAllFromTable();
                 floors.forEach(floorDAO::insert);
             }
             if(locationsImport.isSelected()) {
-                List<Location> locations = CSVFacade.read(Location.class, savedFile.getPath() + "\\" + LOCATION_CSV);
+                List<Location> locations = CSVFacade.read(Location.class, savedFile.getPath() + File.separator + LOCATION_CSV);
                 LocationDAO locationDAO = new LocationDAO();
                 locationDAO.deleteAllFromTable();
                 locations.forEach(locationDAO::insert);
@@ -213,14 +213,14 @@ public class CSVComponent {
 
             //Employee
             if(employeesImport.isSelected()) {
-                List<Employee> employees = CSVFacade.read(Employee.class, savedFile.getPath() + "\\" + EMPLOYEE_CSV);
+                List<Employee> employees = CSVFacade.read(Employee.class, savedFile.getPath() + File.separator + EMPLOYEE_CSV);
                 EmployeeDAO employeeDAO = new EmployeeDAO();
                 employeeDAO.deleteAllFromTable();
                 employees.forEach(employeeDAO::insert);
             }
 
             if(patientImport.isSelected()){
-                List<Patient> patients = CSVFacade.read(Patient.class, savedFile.getPath()+ "\\" + PATIENT_CSV);
+                List<Patient> patients = CSVFacade.read(Patient.class, savedFile.getPath()+ File.separator + PATIENT_CSV);
                 PatientDAO patientDAO = new PatientDAO();
                 patientDAO.deleteAllFromTable();
                 patients.forEach(patientDAO::insert);
@@ -228,7 +228,7 @@ public class CSVComponent {
 
             //Medical Equipment Entity
             if(medicalEquipmentEntityImport.isSelected()) {
-                List<MedicalEquipment> medicalEquipments = CSVFacade.read(MedicalEquipment.class, savedFile.getPath() + "\\" + MEDICAL_EQUIPMENT_ENTITY_CSV);
+                List<MedicalEquipment> medicalEquipments = CSVFacade.read(MedicalEquipment.class, savedFile.getPath() + File.separator + MEDICAL_EQUIPMENT_ENTITY_CSV);
                 MedicalEquipmentDAO medicalEquipmentDAO = new MedicalEquipmentDAO();
                 medicalEquipmentDAO.deleteAllFromTable();
                 medicalEquipments.forEach(medicalEquipmentDAO::insert);
@@ -236,43 +236,43 @@ public class CSVComponent {
 
             //Service Request
             if(medicalEquipmentImport.isSelected()) {
-                List<MedicalEquipmentSR> medicalEquipmentSRS = CSVFacade.read(MedicalEquipmentSR.class, savedFile.getPath() + "\\" + MEDICAL_EQUIPMENT_CSV);
+                List<MedicalEquipmentSR> medicalEquipmentSRS = CSVFacade.read(MedicalEquipmentSR.class, savedFile.getPath() + File.separator + MEDICAL_EQUIPMENT_CSV);
                 MedicalEquipmentSRDAO medicalEquipmentSRDAO = new MedicalEquipmentSRDAO();
                 medicalEquipmentSRDAO.deleteAllFromTable();
                 medicalEquipmentSRS.forEach(medicalEquipmentSRDAO::insert);
             }
             if(medicineDeliveryImport.isSelected()) {
-                List<MedicineDeliverySR> medicineDeliverySRS = CSVFacade.read(MedicineDeliverySR.class, savedFile.getPath() + "\\" + MEDICINE_DELIVERY_CSV);
+                List<MedicineDeliverySR> medicineDeliverySRS = CSVFacade.read(MedicineDeliverySR.class, savedFile.getPath() + File.separator + MEDICINE_DELIVERY_CSV);
                 MedicineDeliverySRDAO medicineDeliverySRDAO = new MedicineDeliverySRDAO();
                 medicineDeliverySRDAO.deleteAllFromTable();
                 medicineDeliverySRS.forEach(medicineDeliverySRDAO::insert);
             }
             if(sanitationImport.isSelected()) {
-                List<SanitationSR> sanitationSRS = CSVFacade.read(SanitationSR.class, savedFile.getPath() + "\\" + SANITATION_CSV);
+                List<SanitationSR> sanitationSRS = CSVFacade.read(SanitationSR.class, savedFile.getPath() + File.separator + SANITATION_CSV);
                 SanitationSRDAO sanitationSRDAO = new SanitationSRDAO();
                 sanitationSRDAO.deleteAllFromTable();
                 sanitationSRS.forEach(sanitationSRDAO::insert);
             }
             if(labSystemImport.isSelected()) {
-                List<LabSystemSR> labSystemSRS = CSVFacade.read(LabSystemSR.class, savedFile.getPath() + "\\" + LAB_SYSTEM_CSV);
+                List<LabSystemSR> labSystemSRS = CSVFacade.read(LabSystemSR.class, savedFile.getPath() + File.separator + LAB_SYSTEM_CSV);
                 LabSystemSRDAO labSystemSRDAO = new LabSystemSRDAO();
                 labSystemSRDAO.deleteAllFromTable();
                 labSystemSRS.forEach(labSystemSRDAO::insert);
             }
             if(facilityMaintenanceImport.isSelected()) {
-                List<FacilityMaintenanceSR> facilityMaintenanceSRS = CSVFacade.read(FacilityMaintenanceSR.class, savedFile.getPath() + "\\" + FACILITY_MAINTENANCE_CSV);
+                List<FacilityMaintenanceSR> facilityMaintenanceSRS = CSVFacade.read(FacilityMaintenanceSR.class, savedFile.getPath() + File.separator + FACILITY_MAINTENANCE_CSV);
                 FacilityMaintenanceSRDAO facilityMaintenanceSRDAO = new FacilityMaintenanceSRDAO();
                 facilityMaintenanceSRDAO.deleteAllFromTable();
                 facilityMaintenanceSRS.forEach(facilityMaintenanceSRDAO::insert);
             }
             if(securityImport.isSelected()) {
-                List<SecuritySR> securitySRS = CSVFacade.read(SecuritySR.class, savedFile.getPath() + "\\" + SECURITY_CSV);
+                List<SecuritySR> securitySRS = CSVFacade.read(SecuritySR.class, savedFile.getPath() + File.separator + SECURITY_CSV);
                 SecuritySRDAO securitySRDAO = new SecuritySRDAO();
                 securitySRDAO.deleteAllFromTable();
                 securitySRS.forEach(securitySRDAO::insert);
             }
             if(deliveryImport.isSelected()) {
-                List<DeliverySystemSR> deliverySystemSRS = CSVFacade.read(DeliverySystemSR.class, savedFile.getPath() + "\\" + DELIVERY_SYSTEM_CSV);
+                List<DeliverySystemSR> deliverySystemSRS = CSVFacade.read(DeliverySystemSR.class, savedFile.getPath() + File.separator + DELIVERY_SYSTEM_CSV);
                 DeliverySystemSRDAO deliverySystemSRDAO = new DeliverySystemSRDAO();
                 deliverySystemSRDAO.deleteAllFromTable();
                 deliverySystemSRS.forEach(deliverySystemSRDAO::insert);
