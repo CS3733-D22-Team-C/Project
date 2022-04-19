de9541554ec1a8faa79a2e181ad44914a2302514
vshp22 <vpdendukuri@gmail.com>
2022-04-19 19:35:06 -0400

    patients SCB to LabsysSR create+resolve

--- src/main/java/edu/wpi/cs3733/D22/teamC/controller/service_request/lab_system/LabSystemSRInsertResolveController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/service_request/lab_system/LabSystemSRInsertResolveController.java
@@ -4,15 +4,23 @@ import com.jfoenix.controls.JFXComboBox;
 import edu.wpi.cs3733.D22.teamC.controller.service_request.BaseServiceRequestResolveController;
 import edu.wpi.cs3733.D22.teamC.controller.service_request.InsertServiceRequestResolveController;
 import edu.wpi.cs3733.D22.teamC.entity.generic.DAO;
+import edu.wpi.cs3733.D22.teamC.entity.patient.Patient;
+import edu.wpi.cs3733.D22.teamC.entity.patient.PatientDAO;
 import edu.wpi.cs3733.D22.teamC.entity.service_request.ServiceRequestDAO;
 import edu.wpi.cs3733.D22.teamC.entity.service_request.lab_system.LabSystemSR;
 import edu.wpi.cs3733.D22.teamC.entity.service_request.lab_system.LabSystemSRDAO;
+import edu.wpi.cs3733.D22.teamC.models.patient.PatientSelectorWindow;
+import edu.wpi.cs3733.D22.teamC.models.utils.ComponentWrapper;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+import org.controlsfx.control.SearchableComboBox;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class LabSystemSRInsertResolveController extends InsertServiceRequestResolveController<LabSystemSR> implements Initializable {
@@ -20,7 +28,7 @@ public class LabSystemSRInsertResolveController extends InsertServiceRequestReso
     private JFXComboBox<String> labType;
 
     @FXML
-    private TextField patientID;
+    private SearchableComboBox<Patient> patientSComboBox;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -28,22 +36,33 @@ public class LabSystemSRInsertResolveController extends InsertServiceRequestReso
         for (LabSystemSR.LabType labs : LabSystemSR.LabType.values()) {
             labType.getItems().add(labs.toString());
         }
+
+        //Patient ComboBox
+        //Query DB
+        PatientDAO patientDAO = new PatientDAO();
+        List<Patient> patients = patientDAO.getAll();
+
+        ComponentWrapper.initializeComboBox(patientSComboBox, Patient::getFirstName);
+
+        patientSComboBox.getItems().setAll(patients);
     }
 
     @Override
     public void setup(BaseServiceRequestResolveController<LabSystemSR> baseServiceRequestResolveController, LabSystemSR serviceRequest, boolean isEditMode) {
         super.setup(baseServiceRequestResolveController, serviceRequest, isEditMode);
         labType.setDisable(!isEditMode);
-        patientID.setEditable(isEditMode);
+        patientSComboBox.setEditable(isEditMode);
+
+        PatientDAO patientDAO = new PatientDAO();
 
         labType.setPromptText(serviceRequest.getLabType().toString());
-        patientID.setText(serviceRequest.getPatientID());
+        patientSComboBox.setValue(patientDAO.getByID(serviceRequest.getPatientID()));
     }
 
     public boolean requiredFieldsPresent(){
         if(labType.getValue() == null && labType.getPromptText().equals(""))
             return false;
-        if(patientID.getText().equals(""))
+        if(patientSComboBox.getValue() == null)
             return false;
         return true;
     }
@@ -53,7 +72,8 @@ public class LabSystemSRInsertResolveController extends InsertServiceRequestReso
         if(isEditMode){
             if(labType.getValue() != null)
                 serviceRequest.setLabType(LabSystemSR.LabType.valueOf(labType.getValue()));
-            serviceRequest.setPatientID(patientID.getText());
+            if(patientSComboBox.getValue() != null)
+                serviceRequest.setPatientID(patientSComboBox.getId());
         }
     }
 
@@ -70,6 +90,15 @@ public class LabSystemSRInsertResolveController extends InsertServiceRequestReso
     @FXML
     void statusUpdatedKeyEvent(KeyEvent event) {
         statusUpdated();
+    }
+
+    @FXML
+    void goToPatientTable(ActionEvent event) throws IOException {
+        new PatientSelectorWindow((patient -> this.setPatient(patient)));
+    }
+
+    private void setPatient(Patient patient){
+        this.patientSComboBox.setValue(patient);
     }
 
 }
