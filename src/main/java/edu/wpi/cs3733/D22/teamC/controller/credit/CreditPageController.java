ff3fa318343675331bce5326f6c6e4c88c222fd3
goreilly2001 <goreilly@wpi.edu>
2022-04-23 16:31:49 -0400

    Created base credit page and contoller, need to link button to page

---
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/credit/CreditPageController.java
@@ -0,0 +1,26 @@
+package edu.wpi.cs3733.D22.teamC.controller.credit;
+
+import com.jfoenix.controls.JFXButton;
+import edu.wpi.cs3733.D22.teamC.App;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CreditPageController implements Initializable {
+
+    @FXML
+    private JFXButton goBackButton;
+
+    private void onGoBackButtonPressed(ActionEvent actionEvent){
+        App.instance.getStage().close();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
