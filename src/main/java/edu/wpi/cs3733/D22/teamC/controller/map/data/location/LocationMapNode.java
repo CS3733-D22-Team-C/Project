595782fb614126030632d0ee8c655f4277189a39
NickFrangie <51765298+NickFrangie@users.noreply.github.com>
2022-04-24 15:38:50 -0400

    Legend Fix

--- src/main/java/edu/wpi/cs3733/D22/teamC/controller/map/data/location/LocationMapNode.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/map/data/location/LocationMapNode.java
@@ -22,6 +22,7 @@ public class LocationMapNode extends MapNode<Location> {
     private static final String NODE_PATH = "view/map/nodes/location.fxml";
 
     // FXML
+    @FXML private Group root;
     @FXML private Group contextGroup;
     @FXML private Circle locationCircle;
     @FXML private Label iconLabel;
@@ -81,6 +82,9 @@ public class LocationMapNode extends MapNode<Location> {
             iconLabel.setGraphic(glyph);
         }
 
+        public void setVisible(boolean visible) {
+            root.setVisible(visible);
+        }
     //#endregion
 
     //#region State Updating
