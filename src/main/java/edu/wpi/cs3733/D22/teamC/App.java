566d7d62f4a35eb268a6c5b05bdbd522de97bbb3
NickFrangie <51765298+NickFrangie@users.noreply.github.com>
2022-04-23 22:56:51 -0400

    Updated Jar

--- src/main/java/edu/wpi/cs3733/D22/teamC/App.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/App.java
@@ -22,7 +22,6 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
-import edu.wpi.cs3733.D22.teamC.TeamCAPI;
 
 import java.io.IOException;
 
