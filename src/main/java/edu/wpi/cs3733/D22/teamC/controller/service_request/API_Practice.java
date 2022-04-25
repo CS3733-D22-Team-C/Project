0fb0dc16940f58c162af749cda929e898e245f06
mmueting <70529094+mmueting@users.noreply.github.com>
2022-04-24 23:02:47 -0400

    Employee Added Team B

--- src/main/java/edu/wpi/cs3733/D22/teamC/controller/service_request/API_Practice.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/service_request/API_Practice.java
@@ -2,25 +2,32 @@
 package edu.wpi.cs3733.D22.teamC.controller.service_request;
 
 import edu.wpi.cs3733.D22.teamC.controller.service_request.facility_maintenance.ServiceExceptionAPI;
-import edu.wpi.cs3733.D22.teamC.entity.medical_equipment.MedicalEquipment;
-import edu.wpi.cs3733.D22.teamU.MedicineRequest;
+import edu.wpi.cs3733.D22.teamC.entity.employee.Employee;
+import edu.wpi.cs3733.D22.teamC.entity.employee.EmployeeDAO;
 import edu.wpi.cs3733.D22.teamZ.api.exception.ServiceException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import edu.wpi.cs3733.D22.teamE.APIApp;
-import edu.wpi.cs3733.D22.teamE.Run;
 import edu.wpi.cs3733.D22.teamZ.api.API;
 import edu.wpi.cs3733.D22.teamC.TeamCAPI;
-import edu.wpi.cs3733.D22.teamD.API.StartAPI;
-import edu.wpi.cs3733.D22.teamX.api.MealRequestAPI;
+import edu.wpi.cs3733.D22.teamB.api.DatabaseController;
 
 
 import java.io.IOException;
+import java.util.List;
 
 public class API_Practice {
 
     @FXML
     void clickTeamBAPI(ActionEvent event) throws edu.wpi.cs3733.D22.teamB.api.ServiceException, IOException {
+        //Adding Employees
+        DatabaseController iptEmployee = new DatabaseController();
+        List<Employee> employees = new EmployeeDAO().getAll();
+        for(Employee employee: employees){
+            iptEmployee.addEmployee();
+        }
+
+
+        //Running the API
         edu.wpi.cs3733.D22.teamB.api.API apiB = new edu.wpi.cs3733.D22.teamB.api.API();
         apiB.run(0,0,700,700,"", "hello", "goodbye");
     }
