ca47913e1c6443c90dc7439ad23edf39c732960c
Brian <brianshin100@yahoo.com>
2022-04-25 17:27:55 -0400

    Deletion bugfixes

--- src/main/java/edu/wpi/cs3733/D22/teamC/entity/service_request/ServiceRequestDAO.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/entity/service_request/ServiceRequestDAO.java
@@ -1,7 +1,12 @@
 package edu.wpi.cs3733.D22.teamC.entity.service_request;
 
 import edu.wpi.cs3733.D22.teamC.HibernateManager;
+import edu.wpi.cs3733.D22.teamC.SessionManager;
+import edu.wpi.cs3733.D22.teamC.entity.employee.Employee;
 import edu.wpi.cs3733.D22.teamC.entity.generic.DAO;
+import edu.wpi.cs3733.D22.teamC.entity.generic.IDEntity;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 import java.util.List;
 
@@ -12,6 +17,32 @@ public class ServiceRequestDAO extends DAO<ServiceRequest> {
     
     protected Class<ServiceRequest> classType() {
         return ServiceRequest.class;
+    }
+    
+    /**
+     * Override of delete to handle reverse-cascade triggers.
+     * @param obj ServiceRequest
+     * @return True if successful.
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean delete(Object obj) {
+        Session session = SessionManager.getSession();
+        try {
+            session.beginTransaction();
+            ServiceRequest castSR = classType().cast(obj);
+            Query<ServiceRequest> query = session.createQuery("delete from " + classType().getName() + 
+                    " s where s.ID = '" + castSR.getID() + "'");
+            query.executeUpdate();
+            session.getTransaction().commit();
+            session.close();
+            return true;
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            session.close();
+            e.printStackTrace();
+        }
+        return false;
     }
     
     /**
