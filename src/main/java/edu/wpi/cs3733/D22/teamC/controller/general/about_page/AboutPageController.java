1226d0698a234af4b5cbb16fc399b09821cde870
Matthew McAlarney <67719286+MacCode7110@users.noreply.github.com>
2022-04-25 01:50:45 -0400

    Working on SVG Path

--- src/main/java/edu/wpi/cs3733/D22/teamC/controller/general/about_page/AboutPageController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/general/about_page/AboutPageController.java
@@ -1,5 +1,14 @@
 package edu.wpi.cs3733.D22.teamC.controller.general.about_page;
 
-public class AboutPageController {
+import javafx.fxml.Initializable;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AboutPageController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
