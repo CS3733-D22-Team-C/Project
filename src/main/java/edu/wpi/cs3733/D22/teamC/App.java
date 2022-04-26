fed63926314be3d789fe1b143d0947221a570135
JohnMuirhead000 <73247467+JohnMuirhead000@users.noreply.github.com>
2022-04-25 20:45:39 -0400

    TC-189 (#155)
    * all day
    
    * Employee Validators
    
    * did Locations validator
    
    * medical equipment
    
    * patient stuff
    
    * 'add patient' label added
    
    * made location and DOB non-editable
    
    * fixed all data base CRUDS ST number fields only take numbers
    
    * added MFX
    
    * now rows un-highlight when deleted. Also, fixed when buttons are highlighted in service request dashboard
    
    * Small bug fix
    
    * check
    
    Co-authored-by: mmueting <70529094+mmueting@users.noreply.github.com>

--- src/main/java/edu/wpi/cs3733/D22/teamC/App.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/App.java
@@ -67,7 +67,7 @@ public class App extends Application {
         SessionManager.switchDatabase(SessionManager.DBMode.EMBEDDED);
         log.info("Starting Up");
     }
-
+    //Test comment
     @Override
     public void start(Stage primaryStage) {
         // Create singleton instance
