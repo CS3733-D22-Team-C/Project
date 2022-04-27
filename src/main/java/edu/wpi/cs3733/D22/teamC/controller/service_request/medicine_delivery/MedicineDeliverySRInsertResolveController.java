70f321021051933fa636e31e15aeeb4d93cebefa
NickFrangie <51765298+NickFrangie@users.noreply.github.com>
2022-04-27 03:56:56 -0400

    Medicine Delivery SR Bug Fixes (#182)
    * Fixxy fixxy nice and fixxy
    
    * Disabled!

--- src/main/java/edu/wpi/cs3733/D22/teamC/controller/service_request/medicine_delivery/MedicineDeliverySRInsertResolveController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/controller/service_request/medicine_delivery/MedicineDeliverySRInsertResolveController.java
@@ -1,5 +1,7 @@
 package edu.wpi.cs3733.D22.teamC.controller.service_request.medicine_delivery;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.svg.SVGGlyph;
 import edu.wpi.cs3733.D22.teamC.controller.service_request.BaseServiceRequestResolveController;
 import edu.wpi.cs3733.D22.teamC.controller.service_request.InsertServiceRequestResolveController;
 import edu.wpi.cs3733.D22.teamC.entity.generic.DAO;
@@ -7,6 +9,7 @@ import edu.wpi.cs3733.D22.teamC.entity.patient.Patient;
 import edu.wpi.cs3733.D22.teamC.entity.patient.PatientDAO;
 import edu.wpi.cs3733.D22.teamC.entity.service_request.medicine_delivery.MedicineDeliverySR;
 import edu.wpi.cs3733.D22.teamC.entity.service_request.medicine_delivery.MedicineDeliverySRDAO;
+import edu.wpi.cs3733.D22.teamC.fileio.svg.SVGParser;
 import edu.wpi.cs3733.D22.teamC.models.patient.PatientSelectorWindow;
 import edu.wpi.cs3733.D22.teamC.models.utils.ComponentWrapper;
 import javafx.event.ActionEvent;
@@ -22,21 +25,20 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 public class MedicineDeliverySRInsertResolveController extends InsertServiceRequestResolveController<MedicineDeliverySR> implements Initializable {
-
-
-    @FXML
-    private TextField dosage;
-
-    @FXML
-    private TextField medicine;
-
-    @FXML
-    private SearchableComboBox<Patient> patientSComboBox;
+    @FXML private TextField dosage;
+    @FXML private TextField medicine;
+    @FXML private SearchableComboBox<Patient> patientSComboBox;
+    @FXML private JFXButton patientButton;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        SVGParser svgParser = new SVGParser();
+        String patientIcon = svgParser.getPath("static/icons/employee_icon.svg");
+        SVGGlyph patientContent = new SVGGlyph(patientIcon);
+        patientContent.setSize(20);
+        patientButton.setGraphic(patientContent);
+        patientButton.setText("");
 
-        //Patient ComboBox
         //Query DB
         PatientDAO patientDAO = new PatientDAO();
         List<Patient> patients = patientDAO.getAll();
@@ -49,15 +51,15 @@ public class MedicineDeliverySRInsertResolveController extends InsertServiceRequ
     @Override
     public void setup(BaseServiceRequestResolveController<MedicineDeliverySR> baseServiceRequestResolveController, MedicineDeliverySR serviceRequest, boolean isEditMode) {
         super.setup(baseServiceRequestResolveController, serviceRequest, isEditMode);
-        dosage.setEditable(isEditMode);
-        medicine.setEditable(isEditMode);
-        patientSComboBox.setEditable(isEditMode);
+        dosage.setDisable(!isEditMode);
+        medicine.setDisable(!isEditMode);
         patientSComboBox.setDisable(!isEditMode);
+        patientButton.setDisable(!isEditMode);
         
         dosage.setText(serviceRequest.getDosage());
         medicine.setText(serviceRequest.getMedicine());
         patientSComboBox.setValue(serviceRequest.getPatient());
-        patientSComboBox.setPromptText(serviceRequest.getPatient().toString());
+        patientSComboBox.setPromptText(serviceRequest.getPatient().toString());;
     }
 
     public boolean requiredFieldsPresent(){
