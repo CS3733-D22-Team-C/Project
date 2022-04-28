8ecc0b5b69f01e600d3e9342fb6443fde4d1dcfc
JohnMuirhead000 <73247467+JohnMuirhead000@users.noreply.github.com>
2022-04-27 23:12:26 -0400

    Added p hotkey (#204)
    * added the hot key for profile page
    
    * OMG

--- src/main/java/edu/wpi/cs3733/D22/teamC/App.java
+++ src/main/java/edu/wpi/cs3733/D22/teamC/App.java
@@ -112,9 +112,6 @@ public class App extends Application {
                                 drawerContentController.selectedButton(drawerContentController.databaseButton);
                             }
                             break;
-                        case "M" :
-                            setView(USER_PROFILE);
-                            break;
                         case "S" :
                             setView(SERVICE_REQUEST_DASHBOARD);
                             drawerContentController.selectedButton(drawerContentController.serviceRequestsButton);
